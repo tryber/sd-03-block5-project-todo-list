1c36d754c3f0d87550a1569dedc7899fe9704fd8
wenderson belko <contato@wendersonbelko.com>
2020-02-28 19:15:01 -0300

    aualização dos requisitos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var numb = 1;
 function adicionar(){
     let txt_pre_definido = document.getElementById('texto-tarefa').value;
-    let t= document.getElementById("lista-tarefas").innerHTML += `<li class='lista' id='${numb}' onclick="cor(${numb})">${txt_pre_definido} <button class="btnapagar" onclick='apaga(${numb})'></button><hr></li>`;
+    let t= document.getElementById("lista-tarefas").innerHTML += `<li class='lista' id='${numb}' ondblclick="cor(${numb})">${txt_pre_definido} <button class="btnapagar" onclick='apaga(${numb})'></button><hr></li>`;
+    document.getElementById('texto-tarefa').value = "";
     numb++;
  }
                
@@ -20,10 +21,26 @@ function cor(numero){
     let divs = document.getElementsByClassName("lista");
     for (let id = 0; id < divs.length; id++) {
         let ids = divs[id].getAttribute('id');
-        if (ids == numero) {
-            divs[id].style.textDecoration = "line-through";
-            divs[id].style.color = "red";
-        break;
+        if(divs[id].style.color == "red"){
+            divs[id].style.textDecoration = "none";
+            divs[id].style.color = "black";
         }
+         else{
+         if (ids == numero) {
+             divs[id].style.textDecoration = "line-through";
+             divs[id].style.color = "red";
+         break;
+         }
+    }
     }
 }
+function apagarTudo(){
+    let e = document.querySelector("ol"); 
+    let child = e.lastElementChild;  
+    while (child) { 
+        e.removeChild(child); 
+        child = e.lastElementChild; 
+    }
+}
+
+
