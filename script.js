9893cbcb5a75c820bd2ac4451a9f30cddfa4efac
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-02-29 18:59:48 -0300

    BackgroundColor rgb(128,128,128) item da lista com um clique adicionado. Line-through com dblclik e remoção do line-through com dblclick adcionado

--- script.js
+++ script.js
@@ -8,30 +8,28 @@ botaoAdicionar.addEventListener('click', () => {
   const itemLista = document.createElement('li');
   itemLista.innerText = textoTarefa.value;
   listaTarefas.appendChild(itemLista);
-  textoTarefa.value = ''
-  itemListaBackground();
-  itemListaLineThrough();
+  textoTarefa.value = null;
 });
 
 function itemListaBackground() {
-  for (let i = 0; i < listaTarefas.children.length; i += 1) {
-    listaTarefas.children[i].addEventListener('click', (event) => {
-      event.target.style.backgroundColor = 'rgb(128,128,128)';
-    });
-  }
+  listaTarefas.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  });
 }
 
+itemListaBackground();
+
 function itemListaLineThrough() {
-  for (let i = 0; i < listaTarefas.children.length; i += 1) {
-    listaTarefas.children[i].addEventListener('dblclick', (event) => {
-      if (event.target.getAttribute('class') == 'completed') {
-        event.target.classList.remove('completed');
-      } else {
-        event.target.classList.add('completed');
-      }
-    });
-  }
+  listaTarefas.addEventListener('dblclick', (event) => {
+    if (event.target.getAttribute('class') == 'completed') {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  });
 }
+
+itemListaLineThrough();
 
 
 
