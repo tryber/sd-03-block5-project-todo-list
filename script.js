153e59adda6c31b290a767fe7fd478d12a490d92
alcantara2509 <alcantara2509@gmail.com>
2020-03-01 17:10:13 -0300

    line-through

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let lista = document.querySelector("#lista-tarefas");
 let btnCriar = document.querySelector('#criar-tarefa');
 let texto = document.querySelector('#texto-tarefa');
+let btnApagaTudo = document.querySelector('#apaga-tudo');
 
 btnCriar.addEventListener("click", addLista);
 btnCriar.addEventListener("click", limparTexto);
+
 
 function addLista(){
   let texto = document.querySelector('#texto-tarefa');
@@ -16,6 +18,22 @@ function addLista(){
     novoItem.innerHTML = texto.value
 
     document.querySelector("#lista-tarefas").appendChild(novoItem)
+
+    novoItem.addEventListener("click", function() {
+      novoItem.style.backgroundColor = "rgb(128,128,128)"
+    })
+
+    novoItem.addEventListener("dblclick", function() {
+
+    novoItem.className = "completed"
+
+    })
+    
+    btnApagaTudo.addEventListener("click", apagaTudo)
+
+function apagaTudo () {
+  document.querySelector("#lista-tarefas").removeChild(novoItem)
+}
 
   }
 };
