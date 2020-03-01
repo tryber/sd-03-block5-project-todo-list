65ca68e91e00f3c4a6fa67f6143f4c35b67a5a1a
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-03-01 14:09:08 -0300

    Botao Mover Para Cima e Mover Para Baixa adicionados e funcionando

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function itemListaBackground() {
     }
   }); */
   listaTarefas.addEventListener('click', (event) => {
-    
+
     if (event.target.classList.item(0) == 'selecionado' || event.target.classList.item(1) == 'selecionado') {
       event.target.classList.remove('selecionado');
     } else {
@@ -52,7 +52,7 @@ const botaoApagarTudo = document.getElementById('apaga-tudo');
 function apagarLista() {
   botaoApagarTudo.addEventListener('click', () => {
     while (listaTarefas.children.length > 0) {
-      listaTarefas.removeChild(listaTarefas.children[0])
+      listaTarefas.removeChild(listaTarefas.children[0]);
     }
   });
 }
@@ -79,12 +79,58 @@ function apagarSelecionado() {
   botaoApagarSelecionado.addEventListener('click', () => {
     for (let i = listaTarefas.children.length - 1; i >= 0; i -= 1) {
       if (listaTarefas.children[i].classList.item(0) == 'selecionado' || listaTarefas.children[i].classList.item(1) == 'selecionado') {
-        listaTarefas.removeChild(listaTarefas.children[i])
+        listaTarefas.removeChild(listaTarefas.children[i]);
       }
     }
-  })
+  });
 }
 
 apagarSelecionado();
 
+const botaoMoverCima = document.getElementById('mover-cima');
 
+function moverParaCima() {
+  let itemListaTarefasCima = '';
+  botaoMoverCima.addEventListener('click', () => {
+    for (let i = 0; i < listaTarefas.children.length; i += 1) {
+      if (listaTarefas.children[i].classList.item(0) == 'selecionado' || listaTarefas.children[i].classList.item(1) == 'selecionado') {
+        if (i == 0) {
+          alert('Não é possível mover este item');
+        } else {
+          itemListaTarefasCima = listaTarefas.children[i - 1].innerText;
+          listaTarefas.children[i - 1].innerText = listaTarefas.children[i].innerText;
+          listaTarefas.children[i].innerText = itemListaTarefasCima;
+          listaTarefas.children[i].classList.remove('selecionado');
+          listaTarefas.children[i - 1].classList.add('selecionado');
+        }
+      }
+    }
+  });
+}
+
+moverParaCima();
+
+const botaoMoverBaixo = document.getElementById('mover-baixo');
+
+function moverParaBaixo() {
+  let itemListaTarefasBaixo = '';
+  botaoMoverBaixo.addEventListener('click', () => {
+    for(let i = listaTarefas.children.length - 1; i >=0 ; i -= 1) {
+      if (listaTarefas.children[i].classList.item(0) == 'selecionado' || listaTarefas.children[i].classList.item(1) == 'selecionado') {
+        if (i == listaTarefas.children.length - 1) {
+          alert('Não é possível mover este item')
+          console.log(listaTarefas.children.length - 1)
+          console.log(i)
+        } else {
+          itemListaTarefasBaixo = listaTarefas.children[i + 1].innerText;
+          listaTarefas.children[i + 1].innerText = listaTarefas.children[i].innerText;
+          listaTarefas.children[i].innerText = itemListaTarefasBaixo;
+          listaTarefas.children[i].classList.remove('selecionado');
+          listaTarefas.children[i + 1].classList.add('selecionado');
+        }
+      }
+    }
+  });
+}
+
+moverParaBaixo();
