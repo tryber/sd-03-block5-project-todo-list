df79c7b77f23b7946f3259c4bf58de0be4c90fa9
alcantara2509 <alcantara2509@gmail.com>
2020-03-01 16:02:30 -0300

    limpando campo

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const lista = document.querySelector('#lista-tarefas');
-const btnCriar = document.querySelector('#criar-tarefa');
-const texto = document.querySelector('#texto-tarefa');
+let lista = document.querySelector("#lista-tarefas");
+let btnCriar = document.querySelector('#criar-tarefa');
+let texto = document.querySelector('#texto-tarefa');
 
 btnCriar.addEventListener("click", addLista)
-
+btnCriar.addEventListener("click", function() {
+  document.querySelector('#texto-tarefa').value=' '
+})
 function addLista(){
 let novoItem = document.createElement('li')
 novoItem.innerHTML = texto.value
 
 document.querySelector("#lista-tarefas").appendChild(novoItem)
+
 }
