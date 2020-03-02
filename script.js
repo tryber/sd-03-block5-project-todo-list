752b4d27e112a9edd2d270062c526225725ccc7e
ilan274 <ilan.herbach@gmail.com>
2020-03-01 22:56:48 -0300

    Grey when clicked and comment editorconfig file

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 // Grab objects
-const grabButton = document.getElementById("criar-tarefa");
-const grabInput = document.getElementById("texto-tarefa");
-const grabList = document.getElementById("lista-tarefas");
+const grabButton = document.getElementById('criar-tarefa');
+const grabInput = document.getElementById('texto-tarefa');
+const grabList = document.getElementById('lista-tarefas');
 
 // Event listener
-grabButton.addEventListener("click", addTodo);
+grabButton.addEventListener('click', addTodo);
 
 // Functions
+// Add to list function
 function addTodo() {
-  let createLi = document.createElement("li");
-  createLi.innerText = grabInput.value;
-  grabList.appendChild(createLi);
-  grabInput.value = "";
+	let createLi = document.createElement('li');
+	createLi.innerText = grabInput.value;
+	grabList.appendChild(createLi);
+	grabInput.value = '';
+	// If clicked, color grey
+	for (let i = 0; i < grabList.children.length; i++) {
+		grabList.children[i].addEventListener('click', function(e) {
+			grabList.children[i].style.backgroundColor = 'rgb(128,128,128)';
+		});
+	}
 }
