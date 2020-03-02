70352eab6e485bb07c3ca7dba5b4f1d07e14a48c
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-02 20:00:50 -0300

    requisito 7/2

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-let tarefaInput = document.getElementById("texto-tarefa");
-const botaoTarefa = document.getElementById("criar-tarefa");
-const listaTarefas = document.getElementById("lista-tarefas");
+const tarefaInput = document.getElementById('texto-tarefa');
+const botaoTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const botaoApagaTudo = document.getElementById('apaga-tudo');
 
 function criaTarefa() {
-  let item = document.createElement("li");
+  const item = document.createElement('li');
   item.innerText = tarefaInput.value;
   listaTarefas.appendChild(item);
   tarefaInput.value = null;
 
-  item.addEventListener("dblclick", function(event) {
-    if (event.target.classList.contains("completed")) {
-      event.target.classList.remove("completed");
+  item.addEventListener('dblclick', function (event) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
     } else {
-      event.target.classList.add("completed");
+      event.target.classList.add('completed');
     }
   });
-  item.addEventListener("click", function(event) {
-    if (event.target.classList.contains("selected")) {
-      event.target.classList.remove("selected");
+  item.addEventListener('click', function (event) {
+    if (event.target.classList.contains('selected')) {
+      event.target.classList.remove('selected');
     } else {
-      event.target.classList.add("selected");
+      event.target.classList.add('selected');
     }
   });
 }
 
-botaoTarefa.addEventListener("click", criaTarefa);
+function apagaTudo() {
+    for (let i = 0; i > document.getElementsByTagName('li').length; i += 1) {
+        document.getElementsByTagName('li').remove();
+    }
+}
+
+botaoTarefa.addEventListener('click', criaTarefa);
+botaoApagaTudo.addEventListener('click', apagaTudo);
