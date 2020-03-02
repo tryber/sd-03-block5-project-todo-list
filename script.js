deae0cf435e2c6ff5c663170491d995d1b858095
Gabriel Lucas <dreadpool38@gmail.com>
2020-03-02 16:00:40 -0300

    Mandando os requisitos básicos

--- script.js
+++ script.js
@@ -7,8 +7,31 @@ function addTODO() {
   //adicionando tarefa a lista
   let item = document.createElement('li');
   item.innerText = tarefa;
+  item.className = "item";
+  item.addEventListener('click',function (e) {
+    e.target.style.backgroundColor = 'rgb(128,128,128)';
+  })
+  item.addEventListener("dblclick", completedMark)
+
 
   lista.appendChild(item);
-
-
 }
+
+function completedMark(e) {
+  e.target.classList.add("completed")
+}
+
+function clearAll() {
+  while(lista.firstChild){
+    lista.removeChild(lista.firstChild)
+  }
+}
+
+function clearCompleted() {
+  let completedTasks = document.getElementsByClassName("completed");
+while(completedTasks[0]){
+  lista.removeChild(completedTasks[0])
+}
+}
+
+
