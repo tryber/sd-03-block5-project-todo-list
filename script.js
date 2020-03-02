11a5fdc8d251761df14bba3f876d471fe5c0d5ca
ilan274 <ilan.herbach@gmail.com>
2020-03-01 23:18:28 -0300

    Grey when clicked

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ const grabButton = document.getElementById('criar-tarefa');
 const grabInput = document.getElementById('texto-tarefa');
 const grabList = document.getElementById('lista-tarefas');
 
+// Functions
+function addTodo() {
+  const createLi = document.createElement('li');
+  createLi.innerText = grabInput.value;
+  grabList.appendChild(createLi);
+  grabInput.value = '';
+  // If clicked, color grey
+  for (let i = 0; i < grabList.children.length; i++) {
+    grabList.children[i].addEventListener('click', function(e) {
+      grabList.children[i].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+}
+
 // Event listener
 grabButton.addEventListener('click', addTodo);
-
-// Functions
-// Add to list function
-function addTodo() {
-	let createLi = document.createElement('li');
-	createLi.innerText = grabInput.value;
-	grabList.appendChild(createLi);
-	grabInput.value = '';
-	// If clicked, color grey
-	for (let i = 0; i < grabList.children.length; i++) {
-		grabList.children[i].addEventListener('click', function(e) {
-			grabList.children[i].style.backgroundColor = 'rgb(128,128,128)';
-		});
-	}
-}
