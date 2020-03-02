6266884dc4da2e3b68650ca9f5174eaf72a53bb3
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 16:46:17 -0300

    Item 8

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function itemSelecionado() {
+  const itemClicado = event.srcElement;
+  itemClicado.style.backgroundColor='rgb(128,128,128)';
+}
+
 function armazenaLimpa() {
   const tarefa = document.getElementsByClassName('tarefa')[0];
   const lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
@@ -5,6 +10,7 @@ function armazenaLimpa() {
   if (tarefa.value === '') {
     // do nothing 
   }else {
+    elementos.addEventListener('click',itemSelecionado);
     elementos.innerHTML = tarefa.value;
     lista.appendChild(elementos);
     tarefa.value = '';
