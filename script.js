e4d44a275f02f3f174de1710ee7029145396e7b8
alcantara2509 <alcantara2509@gmail.com>
2020-03-01 21:29:26 -0300

    risco = null

--- script.js
+++ script.js
@@ -4,42 +4,35 @@ let texto = document.querySelector('#texto-tarefa');
 let btnApagaTudo = document.querySelector('#apaga-tudo');
 
 btnCriar.addEventListener("click", addLista);
-//btnCriar.addEventListener("click", limparTexto);
-
 
 function addLista(){
-  let texto = document.querySelector('#texto-tarefa');
-  if (texto.value == 0){
-    texto.style.borderColor = "#fb7f7f"
-  } else {
-    texto.style.borderColor = "initial";
 
     let novoItem = document.createElement('li')
     novoItem.innerText = texto.value
 
-    document.querySelector("#lista-tarefas").appendChild(novoItem)
+    lista.appendChild(novoItem)
 
     texto.value = null;
 
-    novoItem.addEventListener("click", function() {
-      novoItem.style.backgroundColor = "rgb(128,128,128)"
-    })
+    function apagarTudo(){
+      btnApagaTudo.addEventListener("click", function() {
+              lista.removeChild(novoItem)
+            })
+          }
+      apagarTudo();
 
-    novoItem.addEventListener("dblclick", function() {
-
-    novoItem.className = "completed"
-
-    })
-    
-    btnApagaTudo.addEventListener("click", apagaTudo)
-
-function apagaTudo () {
-  document.querySelector("#lista-tarefas").removeChild(novoItem)
-}
-
-  }
+    function riscar(){
+          novoItem.addEventListener("dblclick", function() {
+            if (novoItem.className == 0){
+            novoItem.className = "completed"  
+          
+          } else { 
+            novoItem.className = null
+        }
+      })
+    }
+    riscar();
 };
 
-/*function limparTexto() {
-  document.querySelector('#texto-tarefa').value=' '
-};*/
+
+
