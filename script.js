2fb6d18e9b66848e2e7d77fd4b90614c41091496
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-02 17:47:25 -0300

    button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
-let items = document.querySelectorAll('li');
 
 button.addEventListener('click', function(){
     if(input.value != ''){
@@ -9,12 +8,14 @@ button.addEventListener('click', function(){
         lista.appendChild(newLi);
         newLi.innerHTML = input.value;
         input.value = '';
-        items = document.querySelectorAll('li');
+        let items = document.querySelectorAll('li');
     }
 });
 
+items = document.querySelectorAll('li');
 [].forEach.call(items, item => {
+
     item.addEventListener('click', function(){
-        this.style.backgroundColor = 'rgb(128,128,128)';
+        this.className = 'selected';
     });
 });
