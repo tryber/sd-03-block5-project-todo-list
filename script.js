59d7a53ba10acfb21024edd55c979839750f0fe4
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-02 17:30:35 -0300

    QUINTO 3

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
+let items = document.querySelectorAll('li');
 
 button.addEventListener('click', function(){
-    let newLi = document.createElement('li');
-    newLi.innerText = input.value;
-    lista.append(newLi);
-    input.value = '';
+    if(input.value != ''){
+        let newLi = document.createElement('li');
+        lista.appendChild(newLi);
+        newLi.innerHTML = input.value;
+        input.value = '';
+        items = document.querySelectorAll('li');
+    }
+});
+
+[].forEach.call(items, item => {
+    item.addEventListener('click', function(){
+        this.style.backgroundColor = 'rgb(128,128,128)';
+    });
 });
