50f08a3a5b63327d67435ad046961c5471c5ad48
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 16:01:45 -0300

    Item 7

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function armazenaLimpa () {
-  let tarefa=document.getElementsByClassName('tarefa')[0];
-  let lista=document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
-  let elementos=document.createElement('li');
+  const tarefa = document.getElementsByClassName('tarefa')[0];
+  let lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+  let elementos = document.createElement('li');
   elementos.innerHTML = tarefa.value;
   lista.appendChild(elementos);
   tarefa.value = '';
