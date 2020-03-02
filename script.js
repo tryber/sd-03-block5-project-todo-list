ef7f112b7f7d66fbfab6abf5da629e91b4915487
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 15:48:53 -0300

    Item 5

---
+++ script.js
@@ -0,0 +1,8 @@
+function armazenaLimpa () {
+  let tarefa=document.getElementsByClassName('tarefa')[0];
+  let lista=document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+  let elementos=document.createElement('li');
+  elementos.innerHTML = tarefa.value;
+  lista.appendChild(elementos);
+  tarefa.value = '';
+}
