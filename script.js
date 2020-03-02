3385a05088fa0e3e3969a45a480a06ea7739424a
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-02 18:43:09 -0300

    requisito 5/2

---
+++ script.js
@@ -0,0 +1,12 @@
+let tarefaInput = document.getElementById("texto-tarefa");
+const botaoTarefa = document.getElementById("criar-tarefa");
+const listaTarefas = document.getElementById("lista-tarefas");
+
+function criaTarefa() {
+  let item = document.createElement("li");
+  item.innerText = tarefaInput.value;
+  listaTarefas.appendChild(item);
+  tarefaInput.value = null;
+}
+
+botaoTarefa.addEventListener("click", criaTarefa);
