4ecf0a37adf3069edb9e559ec1f9aa89dd0ac41d
ilan274 <ilan.herbach@gmail.com>
2020-03-01 21:52:51 -0300

    Verify 5th requirement

---
+++ script.js
@@ -0,0 +1,15 @@
+// Grab objects
+const grabButton = document.getElementById("criar-tarefa");
+const grabInput = document.getElementById("texto-tarefa");
+const grabList = document.getElementById("lista-tarefas");
+
+// Event listeners
+grabButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo() {
+  let createLi = document.createElement("li");
+  createLi.innerText = grabInput.value;
+  grabList.appendChild(createLi);
+  grabInput.value = "";
+}
