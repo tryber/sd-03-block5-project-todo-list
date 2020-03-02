9210263bc940db7a0c52a530bf6ef7efdcc27540
wscatao <wscatao@gmail.com>
2020-03-02 19:14:50 -0300

    Adicionado fundo cinza ao clicar num item da lista

--- script.js
+++ script.js
@@ -4,22 +4,48 @@
 const inputText = document.querySelector('#texto-tarefa');
 const listaOrd = document.querySelector('#lista-tarefas');
 const botaoCriarTarefa = document.querySelector('#criar-tarefa');
+let itemDeLista = null;
+
 
 //eventListeners
 
-botaoCriarTarefa.addEventListener("click", adicionarTarefa)
+botaoCriarTarefa.addEventListener('click', adicionarTarefa)
+
 
 
 //funções
 
 function adicionarTarefa() {
     let textoDigitado = inputText.value;
-    let itemDeLista = document.createElement('li');
-    itemDeLista.innerHTML = textoDigitado;
+    itemDeLista = document.createElement('li');
+    itemDeLista.innerHTML = (textoDigitado);
     listaOrd.appendChild(itemDeLista);
+    adicionaClasseAoLi(itemDeLista);
     limpaCaixaTexto();
+    criarEventListenerLi(itemDeLista);
+}
+
+
+function criarEventListenerLi(li) {
+    li.addEventListener('click', alterarClasse);
+    console.log('Event Listener criado');
 }
 
 function limpaCaixaTexto (){
     inputText.value = '';
+    console.log('input text apagado');
+}
+
+function adicionaClasseAoLi(li){
+    li.classList.add('unchecked');
+}
+
+function alterarClasse(event) {
+    if(event.target.classList == 'unchecked') {
+        event.target.classList.remove('unchecked');
+        event.target.classList.add('checked');
+    } else {
+        event.target.classList.remove('checked');
+        event.target.classList.add('unchecked');
+    }
 }
