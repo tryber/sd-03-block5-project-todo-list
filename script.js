118fb65ed154de6d7a899e4a9f7743627d21a6bd
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 16:29:01 -0300

    Item 7

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function armazenaLimpa () {
+function armazenaLimpa() {
   const tarefa = document.getElementsByClassName('tarefa')[0];
-  let lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
-  let elementos = document.createElement('li');
-  elementos.innerHTML = tarefa.value;
-  lista.appendChild(elementos);
-  tarefa.value = '';
+  const lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+  const elementos = document.createElement('li');
+  if (tarefa.value === '') {
+    // do nothing 
+  }else {
+    elementos.innerHTML = tarefa.value;
+    lista.appendChild(elementos);
+    tarefa.value = '';
+  }
 }
 
-function mudaIcone () {
-    const muda = document.getElementsByClassName('button');
-    for (let i=0; i<muda.length; i+=1) {
-      muda[i].style.cursor = 'pointer';
-    }
+function mudaIcone() {
+  const muda = document.getElementsByClassName('button');
+  muda[0].style.cursor = 'pointer';
+  muda[1].style.cursor = 'pointer';
+  muda[2].style.cursor = 'pointer';
 }
