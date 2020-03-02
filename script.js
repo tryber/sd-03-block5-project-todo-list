26be9d6d624c7aeeddba142de23a6eb84bf3d986
ilan274 <ilan.herbach@gmail.com>
2020-03-02 00:27:51 -0300

    Grey when clicked

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ function addTodo() {
   createLi.innerText = grabInput.value;
   grabList.appendChild(createLi);
   grabInput.value = '';
-  // If clicked, color grey
-  for (let i = 0; i < grabList.children.length; i++) {
-    grabList.children[i].addEventListener('click', function(e) {
-      if (grabList.children[i].className != 'selected') {
-        grabList.children[i].className += 'selected';
-      }
-    });
-  }
+  createLi.addEventListener('dblclick', function(e) {
+    if (createLi.classList.contains('line-through')) {
+      createLi.classList.remove('line-through');
+    } else {
+      createLi.classList.add('line-through');
+    }
+  });
+  createLi.addEventListener('click', function(e) {
+    if (createLi.classList.contains('selected')) {
+    } else {
+      createLi.classList.add('selected');
+    }
+  });
 }
 
 // Event listener
