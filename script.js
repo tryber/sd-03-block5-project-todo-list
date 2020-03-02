11d4bd3a510ec2c91977f80dcac55bbb0be0d0db
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-02 19:15:36 -0300

    requisito 7

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const listaTarefas = document.getElementById('lista-tarefas');
 function criaTarefa() {
   let item = document.createElement('li');
   item.innerText = tarefaInput.value;
+  item.className = 'listItem'
   listaTarefas.appendChild(item);
   tarefaInput.value = null;
 }
 
 botaoTarefa.addEventListener('click', criaTarefa);
 
+const item = document.getElementsByTagName('li');
 
+function coloreFundo() {
+    item.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+item.addEventListener('click', coloreFundo);
