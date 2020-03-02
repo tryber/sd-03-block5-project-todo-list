70a891c03a42d449bbad8e36d6eb46b43ad8e330
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 17:44:13 -0300

    Item 9 e 10

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 function testa() {
-    console.log('funciona');
+  console.log('funciona');
 }
 
 function itemSelecionado() {
   const itemClicado = event.srcElement;
-  if (itemClicado.className === 'item-selecionado item-taxado') {
+  if (itemClicado.className === 'item-selecionado completed') {
     // do nothing
-  }
-  else {
+  } else {
     itemClicado.className = 'item-selecionado';
   }
 }
 
 function taxaItem() {
   const itemClicado = event.srcElement;
-  if (itemClicado.className === 'item-selecionado item-taxado') {
+  if (itemClicado.className === 'item-selecionado completed') {
     itemClicado.className = 'item-selecionado';
-  }
-  else if (itemClicado.className === 'item-selecionado') {
-    itemClicado.className += ' item-taxado';
-  }
-  else if (itemClicado.className === '') {
-    itemClicado.className = 'item-taxado';
+  } else if (itemClicado.className === 'item-selecionado') {
+    itemClicado.className += ' completed';
+  } else if (itemClicado.className === '') {
+    itemClicado.className = 'completed';
   }
 }
 
@@ -30,10 +27,10 @@ function armazenaLimpa() {
   const lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
   const elementos = document.createElement('li');
   if (tarefa.value === '') {
-    // do nothing 
-  }else {
-    elementos.addEventListener('click',itemSelecionado);
-    elementos.addEventListener('dblclick',taxaItem);
+    // do nothing
+  } else {
+    elementos.addEventListener('click', itemSelecionado);
+    elementos.addEventListener('dblclick', taxaItem);
     elementos.innerHTML = tarefa.value;
     lista.appendChild(elementos);
     tarefa.value = '';
@@ -45,4 +42,9 @@ function mudaIcone() {
   muda[0].style.cursor = 'pointer';
   muda[1].style.cursor = 'pointer';
   muda[2].style.cursor = 'pointer';
+}
+
+function apagaTudo() {
+    const apagar = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+    apagar.innerHTML='';
 }
