1f87a113fbdd0228821cbd4f409c559eef39799a
potew <potew0@gmail.com>
2020-03-03 13:31:16 -0300

    Alguns ítens obrigatórios

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 var lista = document.getElementById("lista-tarefas");
-var textoCaixa = document.getElementById("texto-tarefa").value;
 //document.getElementById("criar-tarefa").addEventListener("click",insereitem());
 
 function insereItem() {
+    //  Abaixo, uma gambiarra que impede que a função atribuida seja executada junto com os comandos desta
+    var textoCaixa = document.getElementById("texto-tarefa").value;
     let item = document.createElement("li");
-    item.textContent = textoCaixa;
+    item.innerText = textoCaixa;
     item.onclick = "mudaCorFundo()";
+    item.addEventListener('click', function () { mudaCorFundo(item) });
+    item.addEventListener('dblclick', function () { marcaCompletado(item) });
     lista.appendChild(item);
+    document.getElementById("texto-tarefa").value = "";
 }
-function mudaCorFundo() {
-    //    self.style.backgroundColor = "gray";
-    console.log("Teste");
+
+function mudaCorFundo(item) {
+    item.classList.add("cinza");
+    console.log(item);
+}
+
+function marcaCompletado(item) {
+    item.classList.add("completed");
+    console.log(item);
+}
+
+function apagaFinalizados() {
+    var itemCompletado = document.getElementsByClassName("completed")
+    itemCompletado.remove(self);
+}
+
+function apagaTudo() {
+    lista.removeChild();
 }
