d205c9540bd343d3d749d6fd75bdf9657a171aed
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-03 15:29:03 -0300

    7 consertado e 8 tentaiva

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+function selectItem() {
+  // adiciona background ao item selecionado
+  const selectedItem = document.getElementsByClassName('selected-item')[0];
+  if (selectedItem) selectedItem.classList.remove('selected-item');
+
+  event.target.classList.add('selected-item');  
+}
+
 function makeToDo() {
   // adiciona funcao ao botao
   const inputElement = document.getElementById('texto-tarefa'); 
   const inputedToDo = inputElement.value;
   const list = document.getElementById('lista-tarefas');
   const item = document.createElement('li');
-  
+
   list.appendChild(item);
+  item.addEventListener('click', selectItem);
   item.innerText = inputedToDo;
-  inputElement.value = ""
+
+  inputElement.value = '';
 }
 
 function ONLOAD() {
   const buttonMakeToDo = document.getElementById('criar-tarefa');
-  buttonMakeToDo.addEventListener('click',makeToDo);
+  buttonMakeToDo.addEventListener('click', makeToDo);
 }
 
-window.onload = ONLOAD()
+window.onload = ONLOAD();
