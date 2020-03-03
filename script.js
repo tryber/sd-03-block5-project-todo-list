1b2ec07f30f8a39ad6b1cfafe9d1f269e81b39ad
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 15:16:24 -0300

    Adicionando evento que troca cor de fundo para cinza ao clicar nos elementos da lista

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 document.getElementById("criar-tarefa").addEventListener("click", () => {
   let li = document.createElement("li")
   li.textContent = document.getElementById("texto-tarefa").value
-  li.addEventListener("dblclick", () => li.style.textDecoration = "line-through")
+  li.addEventListener("dblclick", () => { li.style.textDecoration = "line-through" })
+  li.addEventListener("click", () => { li.style.backgroundColor = rgb(128,128,128) })
   document.getElementById("texto-tarefa").value = ''
   document.getElementById("lista-tarefas").appendChild(li)
 })
