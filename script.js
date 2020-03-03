585363dc4edfc14382b20e2275070b02d67a2642
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 16:52:29 -0300

    Adicionando funcao que salva lista

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
-document.getElementById("criar-tarefa").addEventListener("click", () => {
+const createLine = (text) => {
   let li = document.createElement("li")
-  li.textContent = document.getElementById("texto-tarefa").value
+  li.textContent = text
   li.addEventListener("dblclick", () => { (li.classList[0] == "completed") ? li.classList.remove("completed") : li.classList.add("completed"); })
   li.addEventListener("click", () => { li.style.backgroundColor = "rgb(128,128,128)" })
   li.style.cursor = "pointer"
-  document.getElementById("texto-tarefa").value = ''
   document.getElementById("lista-tarefas").appendChild(li)
+}
+
+document.getElementById("criar-tarefa").addEventListener("click", () => {
+  createLine(document.getElementById("texto-tarefa").value)
+  document.getElementById("texto-tarefa").value = ''
 })
 
 document.getElementById("apaga-tudo").addEventListener("click", () => {
@@ -16,3 +20,14 @@ document.getElementById("apaga-tudo").addEventListener("click", () => {
 document.getElementById("remover-finalizados").addEventListener("click", () => {
   document.querySelectorAll("li").forEach( function (e) { if (e.classList[0] == "completed") { e.remove() }})
 })
+
+document.getElementById("salvar-tarefas").addEventListener("click", () => {
+  let todoList = []
+  document.querySelectorAll("li").forEach(e => { todoList.push(e.innerHTML) })
+  window.localStorage.setItem("Todo-array", todoList)
+})
+
+window.localStorage.getItem("Todo-array").split(',').forEach( e => {
+  createLine(e)
+})
+
