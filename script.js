fd69844fb4fb6e2b98ec393e62f22c1a5ca7a082
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-02 23:53:36 -0300

    code climate

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const tarefaInput = document.getElementById("texto-tarefa");
-const botaoTarefa = document.getElementById("criar-tarefa");
-const listaTarefas = document.getElementById("lista-tarefas");
-const botaoApagaTudo = document.getElementById("apaga-tudo");
-const botaoApagaFinalizados = document.getElementById("remover-finalizados");
-const botaoApagaSelecionados = document.getElementById("remover-selecionado");
-const botaoSalva = document.getElementById("salvar-tarefas");
-const botaoMoveCima = document.getElementById("mover-cima");
-const botaoMoveBaixo = document.getElementById("mover-baixo");
+const tarefaInput = document.getElementById('texto-tarefa');
+const botaoTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const botaoApagaTudo = document.getElementById('apaga-tudo');
+const botaoApagaFinalizados = document.getElementById('remover-finalizados');
+const botaoApagaSelecionados = document.getElementById('remover-selecionado');
+const botaoSalva = document.getElementById('salvar-tarefas');
+const botaoMoveCima = document.getElementById('mover-cima');
+const botaoMoveBaixo = document.getElementById('mover-baixo');
 
 function criaTarefa() {
-  const item = document.createElement("li");
+  const item = document.createElement('li');
   item.innerText = tarefaInput.value;
   listaTarefas.appendChild(item);
   tarefaInput.value = null;
 
-  item.addEventListener("dblclick", function() {
-    if (item.classList.contains("completed")) {
-      item.classList.remove("completed");
+  item.addEventListener('dblclick', function () {
+    if (item.classList.contains('completed')) {
+      item.classList.remove('completed');
     } else {
-      item.classList.add("completed");
+      item.classList.add('completed');
     }
   });
-  item.addEventListener("click", function() {
-    if (item.classList.contains("selected")) {
-      item.classList.remove("selected");
+  item.addEventListener('click', function () {
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected');
     } else {
-      item.classList.add("selected");
+      item.classList.add('selected');
     }
   });
 }
 
 function apagaTudo() {
-  const lista = document.getElementsByTagName("li");
+  const lista = document.getElementsByTagName('li');
   const elementos = lista.length - 1;
   for (let i = elementos; i >= 0; i -= 1) {
     lista[i].remove();
@@ -39,50 +39,50 @@ function apagaTudo() {
 }
 
 function apagaFinalizados() {
-  const lista = document.getElementsByTagName("li");
+  const lista = document.getElementsByTagName('li');
   const elementos = lista.length - 1;
   for (let i = elementos; i >= 0; i -= 1) {
-    if (lista[i].classList.contains("completed")) {
+    if (lista[i].classList.contains('completed')) {
       lista[i].remove();
     }
   }
 }
 
 function apagaSelecionados() {
-  const lista = document.getElementsByTagName("li");
+  const lista = document.getElementsByTagName('li');
   const elementos = lista.length - 1;
   for (let i = elementos; i >= 0; i -= 1) {
-    if (lista[i].classList.contains("selected")) {
+    if (lista[i].classList.contains('selected')) {
       lista[i].remove();
     }
   }
 }
 
 function moverCima() {
-  const ol = document.querySelector("ol");
-  const lista = document.querySelectorAll("li");
+  const ol = document.querySelector('ol');
+  const lista = document.querySelectorAll('li');
   const elementos = lista.length - 1;
   for (let i = elementos; i > 0; i -= 1) {
-    if (lista[i].classList.contains("selected")) {
+    if (lista[i].classList.contains('selected')) {
       ol.insertBefore(lista[i], lista[i - 1]);
     }
   }
 }
 
 function moverBaixo() {
-  const ol = document.querySelector("ol");
-  const lista = document.querySelectorAll("li");
+  const ol = document.querySelector('ol');
+  const lista = document.querySelectorAll('li');
   const elementos = lista.length - 1;
   for (let i = elementos; i >= 0; i -= 1) {
-    if (lista[i].classList.contains("selected")) {
+    if (lista[i].classList.contains('selected')) {
       ol.insertBefore(lista[i - 1], lista[i]);
     }
   }
 }
 
-botaoTarefa.addEventListener("click", criaTarefa);
-botaoApagaTudo.addEventListener("click", apagaTudo);
-botaoApagaFinalizados.addEventListener("click", apagaFinalizados);
-botaoApagaSelecionados.addEventListener("click", apagaSelecionados);
-botaoMoveCima.addEventListener("click", moverCima);
-botaoMoveBaixo.addEventListener("click", moverBaixo);
+botaoTarefa.addEventListener('click', criaTarefa);
+botaoApagaTudo.addEventListener('click', apagaTudo);
+botaoApagaFinalizados.addEventListener('click', apagaFinalizados);
+botaoApagaSelecionados.addEventListener('click', apagaSelecionados);
+botaoMoveCima.addEventListener('click', moverCima);
+botaoMoveBaixo.addEventListener('click', moverBaixo);
