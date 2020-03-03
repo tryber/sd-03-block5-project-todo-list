18cae429fdd60b7dc557378186c5fcf1dbd59a30
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-03 10:34:20 -0300

    Taking off button close

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-
-for (let i = 0; i < myNodelist.length; i++) {
-  let btnclose = document.createElement("button");
-  let txt = document.createTextNode("\u00D7");
-  btnclose.className = "close";
-  btnclose.appendChild(txt);
-  myNodelist[i].appendChild(btnclose);
-}
-
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
 // Add a "completed" symbol when clicking on a list item
 let list = document.querySelector('ol');
 list.addEventListener('dblclick', function(ev) {
@@ -26,6 +5,7 @@ list.addEventListener('dblclick', function(ev) {
     ev.target.classList.toggle('completed');
   }
 }, false);
+
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
       ev.target.classList.toggle('selected');
@@ -44,19 +24,6 @@ function newElement() {
     document.getElementById("lista-tarefas").appendChild(li);
   }
   document.getElementById("texto-tarefa").value = "";
-
-  let btnclose = document.createElement("button");
-  let txt = document.createTextNode("\u00D7");
-  btnclose.className = "close";
-  btnclose.appendChild(txt);
-  li.appendChild(btnclose);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }
 
 const apagatudo = document.getElementById('apaga-tudo');
