6ee6a721c43ae722d6b6a6669df0da11c15bfa14
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-03 16:39:32 -0300

    Adding events to two last buttons

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ addItem.addEventListener("click", function(){
     let newItem = document.createElement("li");
     newItem.innerText = newItemContent;
     document.getElementById("lista-tarefas").appendChild(newItem);
-    newItem.addEventListener('click', function(){
+    document.querySelector("#texto-tarefa").value = "";
+    newItem.addEventListener("dblclick", function(){
         newItem.classList.toggle("completed");
         if (newItem.classList == "completed") {
             newItem.style.textDecoration = "line-through";
         } else {
-            newItem.style.textDecoration = "none";
+            newItem.style.removeProperty("text-decoration");
         }
     })
+    newItem.addEventListener("click", function(){
+        newItem.classList.toggle("gray");
+        if (newItem.classList == "gray") {
+            newItem.style.backgroundColor = "rgb(128,128,128)";
+        } else {
+            newItem.style.removeProperty("background-color");
+        }
+    })
+})
+
+let clearAll = document.querySelector("#apaga-tudo");
+clearAll.addEventListener("click", function(){
+    let itemList = document.querySelector("#lista-tarefas");
+    itemList.innerHTML = "";
+})
+
+let removeCompleted = document.querySelector("#remover-finalizados");
+removeCompleted.addEventListener("click", function(){
+    let elementsToRemove = document.getElementsByClassName("completed");
+    while (elementsToRemove.length > 0) {
+        document.querySelector("#lista-tarefas").removeChild(elementsToRemove[0]);
+    }
 })
