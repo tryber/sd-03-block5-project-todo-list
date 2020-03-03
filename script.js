8e1d8e7ceefde80810d85a0ae99937008075ff51
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 14:49:21 -0300

    Adicionando funcoes que adiciona e remove elementos na lista

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+document.getElementById("criar-tarefa").addEventListener("click", () => {
+  let li = document.createElement("li")
+  li.textContent = document.getElementById("texto-tarefa").value
+  document.getElementById("lista-tarefas").appendChild(li)
+})
+
+document.getElementById("apaga-tudo").addEventListener("click", () => {
+  document.querySelectorAll("li").forEach( function (e) {e.remove()})
+})
