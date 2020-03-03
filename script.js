c8312c8f676b4af4bf5c57cb8969ec5b66322fc5
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 15:21:30 -0300

    Fazendo com que o o line through seja retirado caso usuario clique novamente duas vezes

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 document.getElementById("criar-tarefa").addEventListener("click", () => {
   let li = document.createElement("li")
   li.textContent = document.getElementById("texto-tarefa").value
-  li.addEventListener("dblclick", () => { li.style.textDecoration = "line-through" })
-  li.addEventListener("click", () => { li.style.backgroundColor = rgb(128,128,128) })
+  li.addEventListener("dblclick", () => { (li.style.textDecoration == "line-through") ? li.style.textDecoration = "none" : li.style.textDecoration = "line-through"; })
+
+  li.addEventListener("click", () => { li.style.backgroundColor = "rgb(128,128,128)" })
   document.getElementById("texto-tarefa").value = ''
   document.getElementById("lista-tarefas").appendChild(li)
 })
