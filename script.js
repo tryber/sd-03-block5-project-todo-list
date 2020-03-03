697d31b6b701ea9d3d6d4f47dcf2c7a92902513d
fabemiliano <fab.emiliano@gmail.com>
2020-03-03 15:11:11 -0300

    requiste 6

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function addTask() {
     let bullet = document.createElement("li")
     document.getElementById("lista-tarefas").appendChild(bullet)
     bullet.id = "tarefa"
+    bullet.className = "tarefa"
 
     let Task = document.getElementById("texto-tarefa").value
     bullet.innerHTML = Task
@@ -18,3 +19,11 @@ function addTask() {
     
 }
 
+
+let listOfTasks = document.querySelectorAll(".tarefa")
+
+for (let i = 0; i < listOfTasks.length; i++){
+   let taskSelected = listOfTasks[i]
+   taskSelected.getElementsByClassName.background = "red"
+    
+}
