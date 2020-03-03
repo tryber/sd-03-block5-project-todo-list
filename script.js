95ad9c69896fecafdd1e050e336e6718aedfa021
Lourenço <loulage@gmail.com>
2020-03-03 17:22:10 -0300

    Added line-through selected function

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 window.onload = function() {
-
-
-
-    let addTaskBtn = document.getElementById('criar-tarefa');
-
     function createList() {
         //Armazena Conteudo da caixa de input
         //Cria novo elemento li
@@ -11,13 +6,26 @@ window.onload = function() {
         let taskInput = document.getElementById('texto-tarefa').value;
         let liElement = document.createElement("li");
         liElement.innerHTML = taskInput;
-
+        //Adicionar fundo cinza ao elemento.
+        liElement.addEventListener('click', event => {
+            liElement.style.backgroundColor = "rgb(128,128,128)"
+        });
+        //Risca Item selecionado com duplo-click
+        liElement.addEventListener('dblclick', event => {
+            if (liElement.style.textDecoration != "line-through") {
+                liElement.style.textDecoration = "line-through";
+            } else {
+                liElement.style.textDecoration = "none";
+            }
+        });
         //Limpa a caixa de input
         document.getElementById("texto-tarefa").value = '';
         //Adiciona o nó li ao elemento ol
         document.getElementById('lista-tarefas').appendChild(liElement);
 
     }
+
+    let addTaskBtn = document.getElementById('criar-tarefa');
     addTaskBtn.addEventListener("click", createList);
 
 }
