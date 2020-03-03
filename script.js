42722ab92f027bc179fcc7ba0a2b016657cfd4db
tales <talestales@gmail.com>
2020-03-03 18:55:20 -0300

    100

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function clique(event) {
+  event.target.classList.add('cinza');
+}
+
+function doisCliques(event) {
+  event.target.classList.toggle('completed');
+}
 function addTarefa() {
-  let newTarefa = document.getElementById('texto-tarefa').value;
-  const bloco = document.getElementById("lista-tarefas");
+  const newTarefa = document.getElementById('texto-tarefa').value;
+  const bloco = document.getElementById('lista-tarefas');
   const nota = document.createElement('li');
   const texto = document.createTextNode(newTarefa);
   nota.appendChild(texto);
@@ -10,23 +17,15 @@ function addTarefa() {
   document.getElementById('texto-tarefa').value = '';
 }
 
-function clique(event) {
-    event.target.classList.add('cinza');
-}
-
-function doisCliques(event) {
-    event.target.classList.toggle('completed');
-}
-
 function limpar() {
-  let lista = document.querySelector('ol');
-    lista.innerHTML = '';
-  }
+  const lista = document.querySelector('ol');
+  lista.innerHTML = '';
+    }
 
 function limpaSelecao() {
-  let lista = document.querySelector('ol');
-  let item = document.querySelectorAll('.completed');
-    for (let i = 0; i < item.length; i+= 1){
-        lista.removeChild(item[i])
-        }
+  const lista = document.querySelector('ol');
+  const item = document.querySelectorAll('.completed');
+  for (let i = 0; i < item.length; i += 1) {
+      lista.removeChild(item[i]);
     }
+}
