9df30724653786d0db24741e4e4a70189856dad7
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 15:25:36 -0300

    Adicionando botao que remove tarefas finalizadas

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
-
 document.getElementById("criar-tarefa").addEventListener("click", () => {
   let li = document.createElement("li")
   li.textContent = document.getElementById("texto-tarefa").value
   li.addEventListener("dblclick", () => { (li.style.textDecoration == "line-through") ? li.style.textDecoration = "none" : li.style.textDecoration = "line-through"; })
-
   li.addEventListener("click", () => { li.style.backgroundColor = "rgb(128,128,128)" })
   document.getElementById("texto-tarefa").value = ''
   document.getElementById("lista-tarefas").appendChild(li)
@@ -12,4 +10,8 @@ document.getElementById("criar-tarefa").addEventListener("click", () => {
 
 document.getElementById("apaga-tudo").addEventListener("click", () => {
   document.querySelectorAll("li").forEach( function (e) {e.remove()})
+})
+
+document.getElementById("remover-finalizados").addEventListener("click", () => {
+  document.querySelectorAll("li").forEach( function (e) { if (e.style.textDecoration == "line-through") { e.remove() }})
 })
