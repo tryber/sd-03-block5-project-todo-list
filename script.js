461baaac139080431da8770fa99bbf4cc4299219
Marylange <marylangess@gmail.com>
2020-03-03 09:22:03 -0300

    teste2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function adicionarValor() {
   let itensLista = document.getElementById('texto-tarefa').value;
-  let listaTarefaLi = document.createElement('li');
-  listaTarefaLi.innerHTML = itensLista;
+  let li = document.createElement('li');
   let ol = document.getElementById('lista-tarefas');
-  ol.appendChild(listaTarefaLi);
+  ol.appendChild(li);
+  li.textContent = itensLista;
 }
