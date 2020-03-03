96b807807fca5b4818e9c12a1ee3519c7c730710
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-02 22:41:06 -0300

    requisito 10 e 11

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const tarefaInput = document.getElementById('texto-tarefa');
 const botaoTarefa = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const botaoApagaTudo = document.getElementById('apaga-tudo');
+const botaoApagaFinalizados = document.getElementById('remover-finalizados');
 
 function criaTarefa() {
   const item = document.createElement('li');
@@ -9,30 +10,40 @@ function criaTarefa() {
   listaTarefas.appendChild(item);
   tarefaInput.value = null;
 
-  item.addEventListener('dblclick', function (event) {
-    if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
+  item.addEventListener('dblclick', function () {
+    if (item.classList.contains('completed')) {
+      item.classList.remove('completed');
     } else {
-      event.target.classList.add('completed');
+      item.classList.add('completed');
     }
   });
-  item.addEventListener('click', function (event) {
-    if (event.target.classList.contains('selected')) {
-      event.target.classList.remove('selected');
+  item.addEventListener('click', function () {
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected');
     } else {
-      event.target.classList.add('selected');
+      item.classList.add('selected');
     }
   });
 }
 
 function apagaTudo() {
-  let elementos = document.getElementsByTagName('li').length - 1;
-  for (let i = elementos ; i >= 0 ; i += 1) {
-    document.getElementsByTagName('li')[i].remove();
+  const lista = document.getElementsByTagName('li');
+  let elementos = lista.length - 1;
+  for (let i = elementos ; i >= 0 ; i -= 1) {
+    lista[i].remove();
   }
 }
 
-
+function apagaFinalizados() {
+    const lista = document.getElementsByTagName('li');
+    let elementos = lista.length - 1;
+    for (let i = elementos ; i >= 0 ; i -= 1) {
+        if(lista[i].classList.contains('completed')) {
+            lista[i].remove();
+        }
+    }
+}
 
 botaoTarefa.addEventListener('click', criaTarefa);
 botaoApagaTudo.addEventListener('click', apagaTudo);
+botaoApagaFinalizados.addEventListener('click', apagaFinalizados);
