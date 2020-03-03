8381a98f1444610ab4b96f76cdff4b1abad21baf
Rodrigo Brezolin <brezolinr@gmail.com>
2020-03-03 16:37:51 -0300

    adicionando elementos na lista

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-/* let pegaCor = document.querySelectorAll(".color");
-for (let i = 0; i < pegaCor.length; i++) {
-    pegaCor[i].addEventListener("click", function() {
-        document.querySelector(".selected").classList.remove("selected");
-        pegaCor[i].classList.add("selected");
-    });
-}
-
-let pegaPixel = document.querySelectorAll(".pixel");
-for (let i = 0; i < pegaPixel.length; i++) {
-    pegaPixel[i].addEventListener("click", function(){
-        let escolheCor = document.querySelector(".selected");
-        pegaPixel[i].style.backgroundColor = escolheCor.id;
-    });
-}
-
-let apagaTudo = document.getElementById("clear-board");
-apagaTudo.addEventListener("click", function(){
-    let pegaPixel = document.querySelectorAll(".pixel");
-    for(i=0; i < pegaPixel.length; i++){
-        
-        pegaPixel[i].style.backgroundColor = "white";
-    }
-}); */
+let botao = document.querySelector("#enviar");
+botao.addEventListener("click", function(){
+    let pegaTexto = document.querySelector("#texto-tarefa").value;
+    let exibeLista = document.querySelector("#lista");
+    let criaLi = document.createElement("li");
+    criaLi.innerHTML = pegaTexto;
+    exibeLista.appendChild(criaLi);
+});
