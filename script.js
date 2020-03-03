4c4359a7830fbf55e814c112c7f073ed83ebc8b0
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-03 19:16:46 -0300

    js 1

---
+++ script.js
@@ -0,0 +1,12 @@
+let botaoAddTarefa = document.querySelector(".botao-model");
+let caixaDeTexto = document.querySelector("#texto-tarefa");
+let listaDeTarefas = document.querySelector("#lista-tarefas");
+botaoAddTarefa.addEventListener("click", function(){
+    let tarefa = document.createElement("li");
+    listaDeTarefas.appendChild(tarefa);
+    tarefa.innerHTML = caixaDeTexto.value;
+    caixaDeTexto.value = '';
+})
+
+console.log(botaoAddTarefa);
+console.log(caixaDeTexto);
