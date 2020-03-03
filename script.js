e09fbb447fabc80bec661813689ec6052e317dae
fabemiliano <fab.emiliano@gmail.com>
2020-03-03 19:25:34 -0300

    requiste bonus3

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
-
-
 let button = document.getElementById("criar-tarefa")
 button.addEventListener("click", addTask)
 
 function addTask() {
     
-    
+    //criar lista
     let bullet = document.createElement("li")
     document.getElementById("lista-tarefas").appendChild(bullet)
     bullet.id = "tarefa"
@@ -16,25 +13,27 @@ function addTask() {
     bullet.innerHTML = Task
     document.getElementById("texto-tarefa").value = ""
 
-    
+    // adicionar click
     bullet.addEventListener("click", changeColor)
     function changeColor(){
+        bullet.classList.add("selected")
         bullet.style.backgroundColor = "rgb(128,128,128)"
     }
     
+    // adicionar doubleclick
     bullet.addEventListener("dblclick", lineThrough)
     function lineThrough() {
-        if (bullet.classList == "tarefa completed"){
+        if (bullet.classList == "tarefa completed"){   // posso usar o bullet.classList.toggle("class") ou bullet.class.contains("")
             bullet.classList.remove("completed")
         }else{bullet.classList.add("completed")}
         
     }
-}
 
 
-// posso usar o bullet.classList.toggle("class") ou bullet.class.contains("")
 
 
+
+// apagar tudo
 let eraseButton = document.getElementById("apaga-tudo")
 eraseButton.addEventListener("click", deleteAll)
 
@@ -43,6 +42,10 @@ function deleteAll() {
 }
 
 
+
+
+
+//remover completo
 let eraseCompleted = document.getElementById("remover-finalizados")
 eraseCompleted.addEventListener("click", removeCompleted)
 
@@ -52,10 +55,24 @@ function removeCompleted() {
         })
     }
 
+}
+   
 
 
-        
-    
+
+
+
+
+//remover selecionado
+
+let deleteSelected = document.getElementById("remover-selecionado")
+deleteSelected.addEventListener("click", removeSelected)
+
+function removeSelected() {
+    document.querySelectorAll('.selected').forEach(function(selected){
+        selected.remove()
+        })
+    }
 
 
   
