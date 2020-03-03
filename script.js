e93e356b6cb642833390ddbc5f1fe79c42fdbb05
Marylange <marylangess@gmail.com>
2020-03-03 19:12:07 -0300

    teste

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function adicionarItem() {
   let itensLista = document.getElementById('texto-tarefa').value;
   let li = document.createElement('li');
+  li.setAttribute("id", "completed");
   let ol = document.getElementById('lista-tarefas');
   li.textContent = itensLista;
   ol.appendChild(li);
+  li.addEventListener("click", function () {
+    li.style.backgroundColor = "rgb(128, 128, 128)";
+  })
+  li.addEventListener("dblclick", function () {
+    document.getElementById('completed').style.textDecoration = "line-through";
+  })
+  li.removeEventListener("dblclick", function () {
+    document.getElementById('completed').style.textDecoration = "line-through";
+  })
 }
-
-function limparInput() {
-  document.getElementById('texto-tarefa').value = '';
-}
-
-// const btn = document.querySelector(".btn");
-// btn.addEventListener("onclick", function () {
-//   document.querySelector(".btn").style.cursor = "pointer";
-// });
