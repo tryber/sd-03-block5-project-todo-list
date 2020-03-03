17b1a26fd4a9d7d30d5807bd3c9e3274cb55a2f9
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-03 17:17:47 -0300

    COLOR

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ window.onload= function(){
             let li = document.createElement("li");
             let inputValue = texto.value;
             let t = document.createTextNode(inputValue);
+            li.addEventListener("click", function () {li.style.backgroundColor = rgb(128,128,128)})
             li.appendChild(t);
             document.getElementById("lista-tarefas").appendChild(li);
         }
         document.getElementById("texto-tarefa").value = ""
+
     }
     botao.addEventListener("click", addText)
+
+
 }
