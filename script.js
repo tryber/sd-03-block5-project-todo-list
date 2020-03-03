4458cbf3dcbff0767a697dd0ac4c4adf6bd4a34c
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-03 16:40:35 -0300

    10-id colocado, 11-try

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ function selectItem() {
   const selectedItem = document.getElementsByClassName('selected-item')[0];
   if (selectedItem) selectedItem.classList.remove('selected-item');
 
-  event.target.classList.add('selected-item');  
+  event.target.classList.add('selected-item');
+}
+
+function completedItens() {
+  const itemClass = event.target.classList;
+  if (itemClass.contains('completed')) itemClass.remove('completed');
+  else itemClass.add('completed');
 }
 
 function makeToDo() {
   // adiciona funcao ao botao de criar elemento e apagar o texto do input
-  const inputElement = document.getElementById('texto-tarefa'); 
+  const inputElement = document.getElementById('texto-tarefa');
   const inputedToDo = inputElement.value;
   const list = document.getElementById('lista-tarefas');
   const item = document.createElement('li');
@@ -21,25 +27,33 @@ function makeToDo() {
   inputElement.value = '';
 }
 
-function completedItens() {
-  const itemClass = event.target.classList;
-  if (itemClass.contains('completed')) itemClass.remove('completed');
-  else itemClass.add('completed');
-}
 
 function clearList() {
   const list = document.getElementById('lista-tarefas');
   const itens = list.children;
   const total = list.childElementCount;
-  for (let index = total-1; index >= 0; index -= 1) {
-    console.log(itens,index);
+  for (let index = (total - 1); index >= 0; index -= 1) {
     list.removeChild(itens[index]);
+  }
+}
+
+function clearCompleted() {
+  const list = document.getElementById('lista-tarefas');
+  const elements = document.querySelectorAll('#lista-tarefas > li.completed');
+  for (let index = (elements.length - 1); index >= 0; index -= 1) {
+    list.removeChild(elements[index]);
   }
 }
 
 function ONLOAD() {
   const buttonMakeToDo = document.getElementById('criar-tarefa');
   buttonMakeToDo.addEventListener('click', makeToDo);
+
+  const buttonClearAll = document.getElementById('apaga-tudo');
+  buttonClearAll.addEventListener('click', clearList);
+
+  const buttonClearCompleted = document.getElementById('remover-finalizados');
+  buttonClearCompleted.addEventListener('click', clearCompleted);
 }
 
 window.onload = ONLOAD();
