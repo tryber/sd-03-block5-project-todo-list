b515ca122eb5b6c017cfc0ebdedc70ac87074145
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-03 19:33:19 -0300

    apaga tudo

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ window.onload= function(){
             let li = document.createElement("li");
             let inputValue = texto.value;
             let t = document.createTextNode(inputValue);
-            li.classList.add("completed")
+            li.classList.add("incompleted")
             li.addEventListener("click", function () {li.style.backgroundColor = "rgb(128,128,128)"})
+            li.addEventListener("dbclick", function () {li.classList[0]=="completed"?li.classList.remove("completed"):li.classList.add("completed")})
             li.style.cursor="pointer"
             li.appendChild(t);
             document.getElementById("lista-tarefas").appendChild(li);
@@ -17,4 +18,14 @@ window.onload= function(){
         document.getElementById("texto-tarefa").value = ""
     }
     botao.addEventListener("click", addText)
+
+    let apaga=document.getElementById("apaga-tudo")
+    let ap= document.getElementById("lista-tarefas")
+    function apagaTd(){
+        ap.parentNode.removeChild(ap)
+        let apos = document.createElement("ol");
+        apos.classList.add("lista-tarefas")
+        document.getElementsByClassName("apos")[0].appendChild(apos)
+    }
+    apaga.addEventListener("click",apagaTd)
 }
