6709fd4ff72b79ed0a15e31b103dc4836fa13e3e
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-03 17:50:47 -0300

    COLersss

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ window.onload= function(){
             let li = document.createElement("li");
             let inputValue = texto.value;
             let t = document.createTextNode(inputValue);
+            li.classList.add("completed")
             li.addEventListener("click", function () {li.style.backgroundColor = "rgb(128,128,128)"})
+            li.style.cursor="pointer"
             li.appendChild(t);
             document.getElementById("lista-tarefas").appendChild(li);
         }
         document.getElementById("texto-tarefa").value = ""
-
     }
     botao.addEventListener("click", addText)
-
-
 }
