244a2e67a7126e62e4edcc6546ee2a0b98bfc2b0
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 15:01:03 -0300

    teste

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 document.getElementById("criar-tarefa").addEventListener("click", () => {
   let li = document.createElement("li")
   li.textContent = document.getElementById("texto-tarefa").value
+  document.getElementById("texto-tarefa").value = ''
   document.getElementById("lista-tarefas").appendChild(li)
-  document.getElementById("texto-tarefa").value = ""
 })
 
 document.getElementById("apaga-tudo").addEventListener("click", () => {
