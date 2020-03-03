362c341883fc7d43a69f5968f174b4d1ef2c20c6
Jafet Henrique <jafetguerra6@gmail.com>
2020-03-03 15:15:17 -0300

    Adicionando evento ao button

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  let button = document.getElementsByClassName("adicionando-tarefa")[0];
+  let lista = document.getElementsByClassName("lista-ordenada")[0];
+  let input1 = document.getElementsByClassName("tarefa")[0];
+  
+  function listed () {
+    let item1 = document.createElement("li");
+    lista.appendChild(item1);
+    item1.innerHTML = input1.value;
+  }
+  button.addEventListener("click", listed)
+
+
+
+}
