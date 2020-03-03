db44532a8499bcc8793f67f4fcff318913ae5911
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-03 18:07:43 -0300

    Item 11

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
+const lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+
 function testa() {
   console.log('funciona');
 }
 
-function itemSelecionado() {
-  const itemClicado = event.srcElement;
-  if (itemClicado.className === 'item-selecionado completed') {
-    // do nothing
-  } else {
-    itemClicado.className = 'item-selecionado';
-  }
-}
-
-function taxaItem() {
-  const itemClicado = event.srcElement;
-  if (itemClicado.className === 'item-selecionado completed') {
-    itemClicado.className = 'item-selecionado';
-  } else if (itemClicado.className === 'item-selecionado') {
-    itemClicado.className += ' completed';
-  } else if (itemClicado.className === '') {
-    itemClicado.className = 'completed';
-  }
-}
-
 function armazenaLimpa() {
   const tarefa = document.getElementsByClassName('tarefa')[0];
-  const lista = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
   const elementos = document.createElement('li');
   if (tarefa.value === '') {
     // do nothing
@@ -32,8 +13,29 @@ function armazenaLimpa() {
     elementos.addEventListener('click', itemSelecionado);
     elementos.addEventListener('dblclick', taxaItem);
     elementos.innerHTML = tarefa.value;
+    elementos.className = 'elemento';
     lista.appendChild(elementos);
     tarefa.value = '';
+  }
+}
+
+function itemSelecionado() {
+  const itemClicado = event.srcElement;
+  if (itemClicado.className === 'elemento item-selecionado completed') {
+    // do nothing
+  } else {
+    itemClicado.className = 'elemento item-selecionado';
+  }
+}
+
+function taxaItem() {
+  const itemClicado = event.srcElement;
+  if (itemClicado.className === 'elemento item-selecionado completed') {
+    itemClicado.className = 'elemento item-selecionado';
+  } else if (itemClicado.className === 'elemento item-selecionado') {
+    itemClicado.className += ' completed';
+  } else if (itemClicado.className === 'elemento') {
+    itemClicado.className += ' completed';
   }
 }
 
@@ -45,6 +47,24 @@ function mudaIcone() {
 }
 
 function apagaTudo() {
-    const apagar = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
-    apagar.innerHTML='';
+  lista.innerHTML='';
 }
+
+let removeFinalizados = document.getElementById('remover-finalizados');
+
+removeFinalizados.addEventListener('click', function() {
+
+  let elementosFinalizados = document.getElementsByClassName('lista-tarefas')[0].firstElementChild;
+  let filhos = document.getElementsByClassName('elemento');
+  let numeroElementos = filhos.length;
+  let elimina = [];
+  let conta = 0;
+
+  if (elementosFinalizados.lastElementChild != null) {
+    for (let i = numeroElementos-1; i >= 0 ; i -= 1) {
+      if (filhos[i].className === 'elemento item-selecionado completed') {
+        lista.removeChild(filhos[i])
+      }
+    }
+  }
+});
