b63e30246c275a26dd6126c1b9f6fe54b235a07f
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-03 15:48:30 -0300

    algumas alteraçoes resquisitos

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload= function(){
+    let botao=document.getElementById("criar-tarefa")
+    let texto= document.getElementById("texto-tarefa")
+    function addText(){  
+        if (texto === '') {
+          window.alert("OPS, adicione uma tarefa");
+        }else{
+            let li = document.createElement("li");
+            let inputValue = texto.value;
+            let t = document.createTextNode(inputValue);
+            li.appendChild(t);
+            document.getElementById("lista-tarefas").appendChild(li);
+        }
+    }
+    botao.addEventListener("click", addText)
+}
