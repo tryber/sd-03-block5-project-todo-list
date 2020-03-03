1fe833006e1ec7d7a24843e408806caf459f7bc6
potew <potew0@gmail.com>
2020-03-03 18:18:56 -0300

    Botões de apagar

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-var lista = document.getElementById("lista-tarefas");
+var i, lista = document.getElementById("lista-tarefas");
 //document.getElementById("criar-tarefa").addEventListener("click",insereitem());
 
 function insereItem() {
-    //  Abaixo, uma gambiarra que impede que a função atribuida seja executada junto com os comandos desta
+    //  Abaixo, um recurso que impede que a função atribuida seja executada junto com os comandos desta
     var textoCaixa = document.getElementById("texto-tarefa").value;
-    let item = document.createElement("li");
-    item.innerText = textoCaixa;
-    item.onclick = "mudaCorFundo()";
-    item.addEventListener('click', function () { mudaCorFundo(item) });
-    item.addEventListener('dblclick', function () { marcaCompletado(item) });
-    lista.appendChild(item);
-    document.getElementById("texto-tarefa").value = "";
+    if (textoCaixa) {
+        let item = document.createElement("li");
+        item.innerText = textoCaixa;
+        item.onclick = "mudaCorFundo()";
+        item.addEventListener('click', function () { mudaCorFundo(item) });
+        item.addEventListener('dblclick', function () { marcaCompletado(item) });
+        lista.appendChild(item);
+        document.getElementById("texto-tarefa").value = "";
+    }
+    else
+        alert("Digite algo para adicionar");
+    document.getElementById("texto-tarefa").focus();
 }
 
 function mudaCorFundo(item) {
     item.classList.add("cinza");
-    console.log(item);
 }
 
 function marcaCompletado(item) {
-    item.classList.add("completed");
-    console.log(item);
+    if (item.classList.contains("completed"))
+        item.classList.remove("completed");
+    else
+        item.classList.add("completed");
 }
 
+//function apagaItem() {
+//    let itemSelecionado
+//}
+
 function apagaFinalizados() {
-    var itemCompletado = document.getElementsByClassName("completed")
-    itemCompletado.remove(self);
+    var listaCompleta = document.getElementsByClassName("completed");
+    let elementos = listaCompleta.length;
+    while (listaCompleta.length > 0)
+        listaCompleta[0].remove();
 }
 
 function apagaTudo() {
-    lista.removeChild();
+    while (lista.hasChildNodes())
+        lista.removeChild(lista.firstChild);
 }
