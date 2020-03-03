fe575096509b5a7e9e225f4a93dd9e9f2a7cee4d
henrique <hbaetaleite@gmail.com>
2020-03-03 19:37:04 -0300

    Tentando corrigir requisito do botao apaga tudo

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-window.onload = function(){
+
+
   const texto = document.querySelector('#texto-tarefa');
   const botaoTarefa = document.getElementById('criar-tarefa');
-
   const lista = document.getElementById('lista-tarefas');
 
   function listarTarefas () {
     let item1 = document.createElement("li");
-    item1.textContent = texto.value;
     lista.appendChild(item1);
+    item1.textContent = texto.value;
     document.getElementById('texto-tarefa').value = "";
   }
 
   botaoTarefa.addEventListener("click", listarTarefas);
 
   const apagaTudo = document.getElementById('apaga-tudo');
+
   apagaTudo.addEventListener('click', function(){
     location.reload(true);
   });
 
 
-}
+
