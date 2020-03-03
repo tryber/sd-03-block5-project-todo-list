5fa2d46e7225d8e1c7c2d2c1b78ed59059baeb15
Marylange <marylangess@gmail.com>
2020-03-03 16:21:58 -0300

    requisito 7

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function adicionarItem() {
   li.textContent = itensLista;
   ol.appendChild(li);
 }
+
+function limparInput() {
+  document.getElementById('texto-tarefa').value = '';
+}
+
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("onclick", function () {
+//   document.querySelector(".btn").style.cursor = "pointer";
+// });
