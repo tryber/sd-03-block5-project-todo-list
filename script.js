ab37b3c7daa4de4b23bfad8578e55bc5cc39851c
davidepadua <davidepadua1914@gmail.com>
2020-03-03 20:11:19 -0300

    Botao Limpar Lista Adicionado

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ function addTarefa() {
     let conteudoTarefa = document.createTextNode(text);
     novaTarefa.appendChild(conteudoTarefa);
     document.getElementById('lista-tarefas').appendChild(novaTarefa);
+    novaTarefa.classList.add('itemLista');
     document.getElementById('texto-tarefa').value = '';
+}
+
+function apagaTudo() {
+    let a = document.getElementById('lista-tarefas');
+    while (a.firstChild) {
+        a.removeChild(a.firstChild);
+    }
+
+}
+
+function marked() {
+
+
 }
