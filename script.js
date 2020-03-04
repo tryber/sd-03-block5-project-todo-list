0460bd677cce9ae2ae8381a78450e22f4e39810a
luis.campos <gucampos845@gmail.com>
2020-03-04 16:55:25 -0300

    Segunda avaliação

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let botaoCria = document.getElementById('criar-tarefa');
 let botaoApaga = document.getElementById('apaga-tudo')
 let listaOrdenada = document.getElementById('lista-tarefas');
+let texto = document.getElementById('texto-tarefa');
 
 botaoCria.addEventListener('click', function () {
     let novaTarefa = document.createElement('li');
@@ -8,8 +9,20 @@ botaoCria.addEventListener('click', function () {
     let inputTexto = document.getElementById('texto-tarefa').value;
     novaTarefa.innerHTML = inputTexto;
     listaOrdenada.appendChild(novaTarefa);
+    texto.value = " ";
 });
 
 botaoApaga.addEventListener('click', function () {
     listaOrdenada.innerHTML = ' ';
 })
+
+let tarefas = document.querySelectorAll('.tarefa');
+
+for (let i in tarefas.length) {
+    tarefas[i].addEventListener('click', function () {
+        tarefas[i].style.backgroundColor = "#808080"
+    })
+    tarefas[i].addEventListener('dblclick', function () {
+        
+    })
+}
