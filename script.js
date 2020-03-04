73ecee60f3551dea25e6ac4f30010e3584009fd8
Thiago Tavares <thiagooo15@gmail.com>
2020-03-04 18:01:58 -0300

    ol

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const listaTarefa = document.getElementById("lista-tarefa");
-const tarefa = document.getElementById("texto-tarefa")
+const listaTarefa = document.getElementById("lista-tarefas");
+const tarefa = document.getElementById("texto-tarefa");
 
 function addTarefa() {
   const lista = document.createElement("li");
   listaTarefa.appendChild(lista, tarefa);
   lista.innerText = tarefa.value;
 
-  tarefa.value=''
+  tarefa.value = "";
 }
-
