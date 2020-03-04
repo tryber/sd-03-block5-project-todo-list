047dc18149244f3fc9d99e8ef9079644011fcc63
Mateus Moreira <ashuraw@hotmail.com>
2020-03-04 19:11:03 -0300

    v9

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const inputTexto = document.getElementById("texto-tarefa");
 const lista = document.getElementById("lista-tarefas");
 
 function addText() {
-  const listaAdd = document.createElement("li");
+   const listaAdd = document.createElement("li");
   listaAdd.className = "cursor"
   lista.appendChild(listaAdd, inputTexto);
   listaAdd.innerText = inputTexto.value;
   inputTexto.value = "";
+}
+
+function removeList(){
+   for (let i = 0; i < lista.childNodes.length; i += 0){
+      lista.removeChild(lista.childNodes[i]);
+    }
+
 }
