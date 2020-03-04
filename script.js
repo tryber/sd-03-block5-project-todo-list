61e6605d1bd20f098cf1481c4f8a997e07ff2256
luizfernando <luizfernandotic@gmail.com>
2020-03-04 16:06:58 -0300

    ajustes para o codeclimate

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let criartarefa = document.getElementById("criar-tarefa");
+let criartarefa = document.getElementById('criar-tarefa');
 console.log(criartarefa);
-let listaordenada = document.getElementById("lista-tarefas");
+let listaordenada = document.getElementById('lista-tarefas');
 console.log(listaordenada);
 // let inputTextoTarefa = document.getElementById("texto-tarefa");
 // console.log(inputTextoTarefa);
 
 function adicionarItem() {
-  let lista = document.createElement("li");
-  lista.innerHTML = document.getElementById("texto-tarefa").value;
+  let lista = document.createElement('li');
+  lista.innerHTML = document.getElementById('texto-tarefa').value;
   listaordenada.appendChild(lista);
-  document.getElementById("texto-tarefa").value = "";
+  document.getElementById('texto-tarefa').value = '';
 
   // alterar cor de fundo da lista ao clicar no item da lista
   function alterarcordefundodalista() {
-    lista.style.backgroundColor = "rgb(128,128,128)";
+    lista.style.backgroundColor = 'rgb(128,128,128)';
   }
-  lista.addEventListener("click", alterarcordefundodalista)
+  lista.addEventListener('click', alterarcordefundodalista);
 }
 
-criartarefa.addEventListener("click", adicionarItem);
+criartarefa.addEventListener('click', adicionarItem);
