b47d7901fbe842338a307d4a946a00491956b4c5
Marylange <marylangess@gmail.com>
2020-03-04 02:41:08 -0300

    req5.2

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function adicionarItem(event) {
+function adicionarItem() {
   let itensLista = document.getElementById('texto-tarefa').value;
-  let tagLi = document.createElement('li');
-  tagLi.setAttribute("class", "completed corTag");
-  let tagOl = document.getElementById('lista-tarefas');
-  tagLi.textContent = itensLista;
-  tagOl.appendChild(tagLi);
+  let li = document.createElement('li');
+  li.setAttribute("class", "completed completed2");
+  let ol = document.getElementById('lista-tarefas');
+  li.textContent = itensLista;
+  ol.appendChild(li);
   document.getElementById('texto-tarefa').value = "";
-  //style text-decoration e cor background
-}
-document.getElementsByClassName("completed").addEventListener("dblclick", function (event) {
-  event.target.classList.toggle('completed');
-});
 
-document.getElementsByClassName("corTag").addEventListener("click", function () {
-  event.target.classList.toggle('corTag');
-});
+  //cor background li
+  li.addEventListener("click", function () {
+    li.style.backgroundColor = "rgb(128, 128, 128)";
+    event.target.classList.toggle('completed2');
+  })
+
+  //style text-decoration
+  li.addEventListener("dblclick", function () {
+    li.style.textDecoration = "line-through";
+    event.target.classList.toggle('completed');
+  })
+}
