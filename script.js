eefd21509c68433dd2510da7ab5310b9a08c8f90
Marylange <marylangess@gmail.com>
2020-03-04 16:43:35 -0300

    req10

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function adicionarItem() {
   let itensLista = document.getElementById('texto-tarefa').value;
   let li = document.createElement('li');
-  li.setAttribute("class", "completed completed2");
+  li.setAttribute("class", "completed");
   let ol = document.getElementById('lista-tarefas');
   li.textContent = itensLista;
   ol.appendChild(li);
   document.getElementById('texto-tarefa').value = "";
-
   //cor background li
   li.addEventListener("click", function () {
     li.style.backgroundColor = "rgb(128, 128, 128)";
-    event.target.classList.toggle('completed2');
   })
-
   //style text-decoration
   li.addEventListener("dblclick", function () {
     li.style.textDecoration = "line-through";
-    event.target.classList.toggle('completed');
   })
+}
+function apagarTudo() {
+  let elemento = document.getElementById('lista-tarefas').childNodes;
+  document.getElementById('lista-tarefas').remove();
 }
