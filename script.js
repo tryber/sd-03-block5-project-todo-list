6d07afd15e91ceb5df8557a085155c78defb4c3f
Thiago Tavares <thiagooo15@gmail.com>
2020-03-04 19:00:06 -0300

    limpa tela

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const listaTarefa = document.getElementById("lista-tarefas");
 const tarefa = document.getElementById("texto-tarefa");
+const limpar = document.getElementById('apaga-tudo')
 
 function addTarefa() {
   const lista = document.createElement("li");
   lista.className = "cursor"
   listaTarefa.appendChild(lista, tarefa);
   lista.innerText = tarefa.value;
-
   tarefa.value = "";
+}
+
+function limpaLita(){
+  for (let i = 0; i<listaTarefa.childNodes.length; i += 0){
+    listaTarefa.removeChild(listaTarefa.childNodes[i])
+  }
 }
