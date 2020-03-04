a428a328088febee94216c706c6492d6475c6247
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 21:44:14 -0300

    Consertando problemas do code climate

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 const addListeners = (li) => {
-  li.addEventListener("dblclick", () => { (li.classList[0] === "completed") ? li.classList.remove("completed") : li.classList.add("completed"); })
-  li.addEventListener("click", (element) => {
+  li.addEventListener('dblclick', () => { (li.classList[0] === 'completed') ? li.classList.remove('completed') : li.classList.add('completed'); })
+  li.addEventListener('click', (element) => {
     if(document.getElementsByClassName('selected')[0] != null){ document.getElementsByClassName('selected')[0].classList.remove('selected'); }
     element.toElement.classList.add('selected');
   })
-  li.style.cursor = "pointer";
+  li.style.cursor = 'pointer';
 }
 
-document.getElementById("criar-tarefa").addEventListener("click", () => {
-  if(document.getElementById("texto-tarefa").value){
-    let li = document.createElement("li");
-    li.textContent = document.getElementById("texto-tarefa").value;
+document.getElementById('criar-tarefa').addEventListener('click', () => {
+  if(document.getElementById('texto-tarefa').value){
+    let li = document.createElement('li');
+    li.textContent = document.getElementById('texto-tarefa').value;
     addListeners(li);
-    document.getElementById("lista-tarefas").appendChild(li);
-    document.getElementById("texto-tarefa").value = '';
+    document.getElementById('lista-tarefas').appendChild(li);
+    document.getElementById('texto-tarefa').value = '';
   }
 })
 
-document.getElementById("apaga-tudo").addEventListener("click", () => {
-  document.querySelectorAll("li").forEach(function (e) { e.remove() })
+document.getElementById('apaga-tudo').addEventListener('click', () => {
+  document.querySelectorAll('li').forEach(function (e) { e.remove() })
 })
 
-document.getElementById("remover-finalizados").addEventListener("click", () => {
-  document.querySelectorAll("li").forEach(function (e) { if (e.classList.contains("completed")) { e.remove() } });
+document.getElementById('remover-finalizados').addEventListener('click', () => {
+  document.querySelectorAll('li').forEach(function (e) { if (e.classList.contains('completed')) { e.remove() } });
 })
 
-document.getElementById("salvar-tarefas").addEventListener("click", () => {
-  localStorage.setItem("Lista de Tarefas", document.getElementById("lista-tarefas").innerHTML);
+document.getElementById('salvar-tarefas').addEventListener('click', () => {
+  localStorage.setItem('Lista de Tarefas', document.getElementById('lista-tarefas').innerHTML);
 })
 
-if (window.localStorage.getItem("Lista de Tarefas")) {
+if (window.localStorage.getItem('Lista de Tarefas')) {
   document.getElementById('lista-tarefas').innerHTML = window.localStorage.getItem('Lista de Tarefas');
-  document.querySelectorAll("li").forEach ( function (li) {addListeners(li)});
+  document.querySelectorAll('li').forEach ( function (li) {addListeners(li)});
 }
 
-document.getElementById("mover-cima").addEventListener('click', () => {
+document.getElementById('mover-cima').addEventListener('click', () => {
   selected = document.querySelector('.selected');
   if(selected){
     if(selected.previousElementSibling){
@@ -44,7 +44,7 @@ document.getElementById("mover-cima").addEventListener('click', () => {
 
 })
 
-document.getElementById("mover-baixo").addEventListener('click', () => {
+document.getElementById('mover-baixo').addEventListener('click', () => {
   selected = document.querySelector('.selected');
   if(selected){
     if(selected.nextElementSibling){
@@ -53,8 +53,8 @@ document.getElementById("mover-baixo").addEventListener('click', () => {
   }
 })
 
-document.getElementById("remover-selecionado").addEventListener('click', () => {
-  if(document.querySelector(".selected")){
-    document.querySelector(".selected").remove();
+document.getElementById('remover-selecionado').addEventListener('click', () => {
+  if(document.querySelector('.selected')){
+    document.querySelector('.selected').remove();
   }
 })
