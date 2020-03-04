1f6d8b403f5ea73eb6741db486e602c1492e1168
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-03 23:02:15 -0300

    Adjusting to code climate

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ function doubleClick(event) {
   event.target.classList.toggle('completed');
 }
 
-function simpleClick(event){
+function simpleClick() {
   event.target.classList.toggle('grey');
 }
 
-function createItem(event){
-  let newItemContent = document.querySelector('#texto-tarefa').value;
-  let newItem = document.createElement('li');
+function createItem(event) {
+  const newItemContent = document.querySelector('#texto-tarefa').value;
+  const newItem = document.createElement('li');
   newItem.innerText = newItemContent;
   document.getElementById('lista-tarefas').appendChild(newItem);
   document.querySelector('#texto-tarefa').value = '';
-  newItem.addEventListener('dblclick', doubleClick)
-  newItem.addEventListener('click', simpleClick)
+  newItem.addEventListener('dblclick', doubleClick);
+  newItem.addEventListener('click', simpleClick);
 }
 
-function clearList(event){
-  let itemList = document.querySelector('#lista-tarefas');
+function clearList() {
+  const itemList = document.querySelector('#lista-tarefas');
   itemList.innerHTML = '';
 }
 
-function removeCompletedItem(event){
-  let elementsToRemove = document.getElementsByClassName('completed');
+function removeCompletedItem() {
+  const elementsToRemove = document.getElementsByClassName('completed');
   while (elementsToRemove.length > 0) {
     document.querySelector('#lista-tarefas').removeChild(elementsToRemove[0]);
   }
 }
 
-function removeSelectedItem(event){
+function removeSelectedItem() {
   let elementsToRemove = document.getElementsByClassName('grey');
   while (elementsToRemove.length > 0) {
     document.querySelector('#lista-tarefas').removeChild(elementsToRemove[0]);
   }
 }
 
-function moveItemUp(event){
-  let elementToMove = document.querySelector('.grey');
+function moveItemUp() {
+  const elementToMove = document.querySelector('.grey');
   if (elementToMove) {
     if (elementToMove.previousElementSibling) {
       elementToMove.parentNode.insertBefore(elementToMove, elementToMove.previousElementSibling);
@@ -44,8 +44,8 @@ function moveItemUp(event){
   }
 }
 
-function moveItemDown(event){
-  let elementToMove = document.querySelector('.grey');
+function moveItemDown() {
+  const elementToMove = document.querySelector('.grey');
   if (elementToMove) {
     if (elementToMove.nextElementSibling) {
       elementToMove.parentNode.insertBefore(elementToMove.nextElementSibling, elementToMove);
@@ -53,37 +53,37 @@ function moveItemDown(event){
   }
 }
 
-function saveList(event){
-  let currentList = document.querySelector('#lista-tarefas');
+function saveList(event) {
+  const currentList = document.querySelector('#lista-tarefas');
   localStorage.setItem('saveItens', currentList.innerHTML);
 }
 
-let addItem = document.getElementById('criar-tarefa');
+const addItem = document.getElementById('criar-tarefa');
 addItem.addEventListener('click', createItem);
 
-let clearAll = document.querySelector('#apaga-tudo');
+const clearAll = document.querySelector('#apaga-tudo');
 clearAll.addEventListener('click', clearList);
 
-let removeCompleted = document.querySelector('#remover-finalizados');
+const removeCompleted = document.querySelector('#remover-finalizados');
 removeCompleted.addEventListener('click', removeCompletedItem);
 
-let removeSelected = document.querySelector('#remover-selecionado');
+const removeSelected = document.querySelector('#remover-selecionado');
 removeSelected.addEventListener('click', removeSelectedItem);
 
-let moveUp = document.querySelector('#mover-cima');
+const moveUp = document.querySelector('#mover-cima');
 moveUp.addEventListener('click', moveItemUp);
 
-let moveDown = document.querySelector('#mover-baixo');
+const moveDown = document.querySelector('#mover-baixo');
 moveDown.addEventListener('click', moveItemDown);
 
-let currentList = localStorage.saveItens;
+const currentList = localStorage.saveItens;
 if (currentList) {
   document.querySelector('#lista-tarefas').innerHTML = currentList;
-  let savedItens = document.querySelectorAll('li');
-  for (i = 0; i < savedItens.length; i += 1){
+  const savedItens = document.querySelectorAll('li');
+  for (let i = 0; i < savedItens.length; i += 1) {
     savedItens[i].addEventListener('dblclick', doubleClick);
     savedItens[i].addEventListener('click', simpleClick);
   }
 }
-let saveItens = document.querySelector('#salvar-tarefas');
+const saveItens = document.querySelector('#salvar-tarefas');
 saveItens.addEventListener('click', saveList);
