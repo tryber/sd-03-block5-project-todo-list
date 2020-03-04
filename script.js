660e073bcf848d5768642bb43f279b6d9dcd4816
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-03-04 17:37:50 -0300

    Atualização

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const blista = document.getElementById('criar-tarefa');
 const olista = document.getElementById('lista-tarefas');
 const texttarefa = document.getElementById('texto-tarefa');
 const dlista = document.getElementById('apaga-tudo');
-const select = document.getElementsByClassName('selected');
+
+
 
 blista.addEventListener('click', function() {
     const lisTarefa = document.createElement('li');
     olista.appendChild(lisTarefa);
+    lisTarefa.className = "listA"
     lisTarefa.innerText = texttarefa.value;
     texttarefa.value = " ";
-
 });
+
 
 dlista.addEventListener('click', function() {
     olista.innerHTML = " ";
