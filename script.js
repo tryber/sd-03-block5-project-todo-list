dc2dbf117a3c253fae25bad01244634117d6106b
paulo-lins <paulolins100@gmail.com>
2020-03-03 22:53:09 -0300

    aperf.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var botaoCriar = document.getElementById('criar-tarefa')
 var idr = document.getElementById('taskPlace')
 var olId = document.getElementById("lista-tarefas")
 var completed = document.getElementsByClassName('completed')
+var deleteAll = document.getElementById('apaga-tudo')
 
 function type() {
 
@@ -15,22 +16,43 @@ function type() {
   document.getElementById("lista-tarefas").appendChild(li);
   document.getElementById("texto-tarefa").value = '';
   listaDeTarefas.push(li)
-  li.id = 'liId' + i
+  li.id =  i
     ++i
   console.log('criado a li ' + li)
 
 }
 
-function identifyObj(event) {
-  var pickId = event.target.id;
-  document.getElementById(pickId).style.background = 'green'
-}
+// function identifyObj(event) {
+//   var pickId = event.target.id;
+//   document.getElementById(pickId).style.background = 'green'
+// }
 
 function changeColorLi(event) {
   var pickId = event.target.id;
-  if ((document.getElementById(pickId).style.background = 'rgb(128,128,128)' )===true) {document.getElementById(pickId).style.background = 'white' } else {ocument.getElementById(pickId).style.background = 'rgb(128,128,128)' }
+  document.getElementById(pickId).style.background = 'rgb(128,128,128)'
 
 }
+
+function reverseColorLi(event) {
+  var pickId = event.target.id;
+  document.getElementById(pickId).style.background = 'white'
+}
+
+function deleteTasks() {
+  // for (var j = 0; j<listaDeTarefas.length; j += 1){
+    
+  //   var liDel = document.getElementById("lista-tarefas");
+  //   list.removeChild(list.childNodes[j]);
+  //   console.log(j)
+    
+  // } 
+  var elemento = document.getElementById("lista-tarefas");
+while (elemento.firstChild) {
+  elemento.removeChild(elemento.firstChild);
+}
+}
+
+
 
 function pickObj(e) {
   var idSelect = (e.target.id);
@@ -43,3 +65,5 @@ function pickObj(e) {
 botaoCriar.addEventListener('click', type);
 
 olId.addEventListener('click', changeColorLi)
+olId.addEventListener('dblclick', reverseColorLi)
+deleteAll.addEventListener('click', deleteTasks)
