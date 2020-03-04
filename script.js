89856541de1748549a2985392d603656527394c4
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-03-04 15:48:41 -0300

    corrections in the title, and adding effect change in the mouse4

--- script.js
+++ script.js
@@ -3,23 +3,18 @@ let criarTarefa = document.querySelector("#criar-tarefa");
 let textoTarefa = document.querySelector("#texto-tarefa");
 let listaOrdenada = document.querySelector("ol");
 
+
 criarTarefa.addEventListener("click", function textonaLista() {
   let itemLista = document.createElement("li");
-  itemLista.className = 'cursor';
+  itemLista.className = "cursor selected";
   itemLista.innerText = textoTarefa.value;
   listaOrdenada.appendChild(itemLista);
   textoTarefa.value = "";
- });
-
- // apagar tarefas
+});
+// apagar tarefas
 const botaoApagarTudo = document.querySelector("#apaga-tudo");
 
 botaoApagarTudo.addEventListener("click", function apagarTudo() {
   listaOrdenada.innerHTML = "";
 });
-
-const cursor = document.querySelector('.class');
-
-// document.addEventListener('mousemove', e =>{
-//   cursor.setAttribute("style", "top:" +e.pageY+"px; left:"+e.pageX+"px;")
-// })
+//mudar fundo 
