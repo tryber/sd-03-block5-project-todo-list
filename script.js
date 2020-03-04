372f8d5efb35a3e5e007d66bd3dfb1af7cee6047
Thiago Tavares <thiagooo15@gmail.com>
2020-03-04 16:00:50 -0300

    add tarefa

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log('oi');
+const listaTarefa = document.getElementById("lista-tarefa");
+const tarefa = document.getElementById("texto-tarefa")
+
+function addTarefa() {
+  const lista = document.createElement("li");
+  listaTarefa.appendChild(lista, tarefa);
+  lista.innerText = tarefa.value;
+
+  tarefa.value=''
+}
 
