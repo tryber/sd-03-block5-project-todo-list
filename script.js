0b5b0202a0b01126663e67cc75f42dc04f206117
alcantara2509 <alcantara2509@gmail.com>
2020-03-04 15:11:40 -0300

    remover selecionados

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let btnCriar = document.querySelector('#criar-tarefa');
 let texto = document.querySelector('#texto-tarefa');
 let btnApagaTudo = document.querySelector('#apaga-tudo');
 let btnRemoverFinalizados = document.querySelector('#remover-finalizados');
+let btnRemoverSelecionados = document.querySelector('#remover-selecionado');
 
 btnCriar.addEventListener("click", addLista);
 
@@ -36,6 +37,14 @@ function addLista(){
       })
     }
     apagarFinalizados();
+
+    function apagarSelecionados() {
+      btnRemoverSelecionados.addEventListener("click", function() {
+        let selected = document.querySelector(".selected");
+        lista.removeChild(selected)
+      })
+    }
+    apagarSelecionados();
 
     function apagarTudo(){
       btnApagaTudo.addEventListener("click", function() {
