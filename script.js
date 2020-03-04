3516e6a234ec9dd3a75aead8a7b0c6c74e7d8c5f
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 21:48:09 -0300

    Consertando problemas do code climate

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ const addListeners = (li) => {
   li.addEventListener('click', (element) => {
     if (document.getElementsByClassName('selected')[0] != null) { document.getElementsByClassName('selected')[0].classList.remove('selected'); }
     element.toElement.classList.add('selected');
-  })
+  });
   li.style.cursor = 'pointer';
-}
+};
 
 document.getElementById('criar-tarefa').addEventListener('click', () => {
   if (document.getElementById('texto-tarefa').value) {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.textContent = document.getElementById('texto-tarefa').value;
     addListeners(li);
     document.getElementById('lista-tarefas').appendChild(li);
     document.getElementById('texto-tarefa').value = '';
   }
-})
+});
 
 document.getElementById('apaga-tudo').addEventListener('click', () => {
   document.querySelectorAll('li').forEach(function (e) { e.remove() })
-})
+});
 
 document.getElementById('remover-finalizados').addEventListener('click', () => {
   document.querySelectorAll('li').forEach(function (e) { if (e.classList.contains('completed')) { e.remove() } });
-})
+});
 
 document.getElementById('salvar-tarefas').addEventListener('click', () => {
   localStorage.setItem('Lista de Tarefas', document.getElementById('lista-tarefas').innerHTML);
-})
+});
 
 if (window.localStorage.getItem('Lista de Tarefas')) {
   document.getElementById('lista-tarefas').innerHTML = window.localStorage.getItem('Lista de Tarefas');
   document.querySelectorAll('li').forEach(function (li) { addListeners(li) });
-}
+};
 
 document.getElementById('mover-cima').addEventListener('click', () => {
   selected = document.querySelector('.selected');
@@ -42,7 +42,7 @@ document.getElementById('mover-cima').addEventListener('click', () => {
     }
   }
 
-})
+});
 
 document.getElementById('mover-baixo').addEventListener('click', () => {
   selected = document.querySelector('.selected');
@@ -51,10 +51,10 @@ document.getElementById('mover-baixo').addEventListener('click', () => {
       selected.parentNode.insertBefore(selected.nextElementSibling, selected);
     }
   }
-})
+});
 
 document.getElementById('remover-selecionado').addEventListener('click', () => {
   if (document.querySelector('.selected')) {
     document.querySelector('.selected').remove();
   }
-})
+});
