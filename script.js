655c98e4e83daf1ba65487c03bb113a6e00f8d1e
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-03 21:56:01 -0300

    Consertando problemas do code climate

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const addListeners = (li) => {
-  li.addEventListener('dblclick', () => { (li.classList[0] === 'completed') ? li.classList.remove('completed') : li.classList.add('completed'); });
+  li.addEventListener('dblclick', function() { (li.classList[0] === 'completed') ? li.classList.remove('completed') : li.classList.add('completed'); });
   li.addEventListener('click', (element) => {
     if (document.getElementsByClassName('selected')[0] != null) { document.getElementsByClassName('selected')[0].classList.remove('selected'); }
     element.toElement.classList.add('selected');
