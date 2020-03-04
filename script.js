4c0ec0ef470a4dc491815d34b6012889a31915a0
Mateus Moreira <ashuraw@hotmail.com>
2020-03-04 16:01:44 -0300

    v5

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const botaoAdd = document.getElementById("criar-tarefa")
-const inputTexto = document.getElementById("texto-tarefa").value
+const inputTexto = document.getElementById("texto-tarefa")
 const lista = document.getElementById("lista-tarefas")
 
 function addText(){
-   const inputTexto = document.getElementById("texto-tarefa").value
    const listaAdd = document.createElement("li")
    lista.appendChild(listaAdd,inputTexto)
-   listaAdd.innerText = inputTexto
-}
-function limpar (){
-   const inputTexto = document.getElementById("texto-tarefa").value = ''
-
+   listaAdd.innerText = inputTexto.value
+   inputTexto.value = ''
 }
