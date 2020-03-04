9f76a80d97b628a03572a71622fe09ba87068cf9
henrique <hbaetaleite@gmail.com>
2020-03-04 00:26:15 -0300

    Algumas atualizaçoes e testes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function(){
-
   const texto = document.getElementById('texto-tarefa');
   const botaoTarefa = document.getElementById('criar-tarefa');
   const lista = document.getElementById('lista-tarefas');
@@ -8,22 +6,33 @@ window.onload = function(){
     let item1 = document.createElement("li");
     lista.appendChild(item1);
     item1.textContent = texto.value;
+    item1.classList.add('li');
     document.getElementById('texto-tarefa').value = "";
   }
 
   botaoTarefa.addEventListener('click', listarTarefas);
 
-  const apagaTudo = document.getElementById('apaga-tudo');
+  // let liGray = document.getElementsByClassName('li');
 
-  apagaTudo.addEventListener('click', function(){
-    location.reload(true);
-  });
+  // liGray.addEventListener('click', function(){
+  //   liGray.style.backgroundColor = "rgb (128, 128, 128)";
+  // })
 
-  // function removeAll (){
-  //   let lista = document.getElementById("lista-tarefas")
-  //   while (lista.firstChild){
-  //   lista.removeChild(lista.lastChild)
-  //   }
-  //   }
 
+  // corLista.addEventListener('click', function(){
+  //   item1.style.backgroundColor = 'rgb (128, 128, 128)'
+  // })
+
+
+
+  function apagarTudo (){
+    while (lista.firstChild){
+    lista.removeChild(lista.lastChild);
+  }
 }
+
+   const apagaTudo = document.getElementById('apaga-tudo');
+   apagaTudo.addEventListener('click', apagarTudo);
+
+
+    //location.reload(true);
