65f85d08a65e43d18657bba4c2340cc34abb044f
henrique <hbaetaleite@gmail.com>
2020-03-04 11:30:16 -0300

    corrigindo alguns erros do codeclimate

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
-window.onload = function(){
 
-  const texto = document.getElementById('texto-tarefa');
-  const botaoTarefa = document.getElementById('criar-tarefa');
-  const lista = document.getElementById('lista-tarefas');
-  const botaoApagaTudo = document.getElementById('apaga-tudo');
+const texto = document.getElementById('texto-tarefa');
+const botaoTarefa = document.getElementById('criar-tarefa');
+const lista = document.getElementById('lista-tarefas');
+const botaoApagaTudo = document.getElementById('apaga-tudo');
 
-  function listarTarefas (){
-    let item1 = document.createElement("li");
-    lista.appendChild(item1);
-    item1.textContent = texto.value;
-    item1.classList.add('listaTarefa');
-    document.getElementById('texto-tarefa').value = "";
-  };
+function listarTarefas() {
+  const item1 = document.createElement('li');
+  lista.appendChild(item1);
+  item1.textContent = texto.value;
+  item1.classList.add('listaTarefa');
+  document.getElementById('texto-tarefa').value = '';
+}
 
-  botaoTarefa.addEventListener('click', listarTarefas);
+botaoTarefa.addEventListener('click', listarTarefas);
 
-
-  function apagarTudo (){
-    while (lista.firstChild){
-      lista.removeChild(lista.lastChild);
-    }
-  };
-
-  botaoApagaTudo.addEventListener('click', apagarTudo);
-
-
-
-  let lis = document.getElementsByClassName('listaTarefa');
-
-  function addFundoCinza(){
-    for (let l = 0; l < lis.length; l += 1){
-    lis[l].style.backgroundColor = 'rgb(128,128,128)';
-    }
-  };
-
-  lista.addEventListener('click', addFundoCinza);
-
-
-
-
-
-  function addRiscado(){
-    lis.classList.add('completed');
+function apagarTudo() {
+  while (lista.firstChild) {
+    lista.removeChild(lista.lastChild);
   }
+}
 
-  lista.addEventListener('dbclick', addRiscado);
+botaoApagaTudo.addEventListener('click', apagarTudo);
 
+let lis = document.getElementsByClassName('listaTarefa');
+
+function addFundoCinza() {
+  for (let l = 0; l < lis.length; l += 1) {
+  lis[l].style.backgroundColor = 'rgb(128,128,128)';
+  }
 };
+
+lista.addEventListener('click', addFundoCinza);
+
+function addRiscado(){
+  lis.classList.add('completed');
+  lis.style.textDecorationLine = 'lineThrough';
+}
+
+lista.addEventListener('dbclick', addRiscado);
