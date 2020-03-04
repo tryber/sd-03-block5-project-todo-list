ee30bab04f13ea1690bfe62ccc3c5361aa0e2b9b
fabemiliano <fab.emiliano@gmail.com>
2020-03-03 23:06:54 -0300

    requiste bonus3 again

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let button = document.getElementById("criar-tarefa")
 button.addEventListener("click", addTask)
 
+
 function addTask() {
-    
+
     //criar lista
     let bullet = document.createElement("li")
     document.getElementById("lista-tarefas").appendChild(bullet)
-    bullet.id = "tarefa"
     bullet.className = "tarefa"
 
     let Task = document.getElementById("texto-tarefa").value
@@ -15,64 +15,66 @@ function addTask() {
 
     // adicionar click
     bullet.addEventListener("click", changeColor)
-    function changeColor(){
+    function changeColor() {
         bullet.classList.add("selected")
         bullet.style.backgroundColor = "rgb(128,128,128)"
     }
-    
+
     // adicionar doubleclick
     bullet.addEventListener("dblclick", lineThrough)
     function lineThrough() {
-        if (bullet.classList == "tarefa completed"){   // posso usar o bullet.classList.toggle("class") ou bullet.class.contains("")
+        if (bullet.classList == "tarefa completed" || bullet.classList == "tarefa selected completed") {   // posso usar o bullet.classList.toggle("class") ou bullet.class.contains("")
             bullet.classList.remove("completed")
-        }else{bullet.classList.add("completed")}
-        
+        } else { bullet.classList.add("completed") }
+
+    }
+    // apagar tudo
+    let eraseButton = document.getElementById("apaga-tudo")
+    eraseButton.addEventListener("click", deleteAll)
+
+    function deleteAll() {
+        document.getElementById("lista-tarefas").innerHTML = ""
     }
 
+    //remover completo
+    let eraseCompleted = document.getElementById("remover-finalizados")
+    eraseCompleted.addEventListener("click", removeCompleted)
 
-
-
-
-// apagar tudo
-let eraseButton = document.getElementById("apaga-tudo")
-eraseButton.addEventListener("click", deleteAll)
-
-function deleteAll() {
-    document.getElementById("lista-tarefas").innerHTML = ""
-}
-
-
-
-
-
-//remover completo
-let eraseCompleted = document.getElementById("remover-finalizados")
-eraseCompleted.addEventListener("click", removeCompleted)
-
-function removeCompleted() {
-    document.querySelectorAll('.completed').forEach(function(selected){
-        selected.remove()
+    function removeCompleted() {
+        document.querySelectorAll('.completed').forEach(function (selected) {
+            selected.remove()
         })
     }
 
-}
-   
+    //remover selecionado
 
+    let deleteSelected = document.getElementById("remover-selecionado")
+    deleteSelected.addEventListener("click", removeSelected)
 
+    function removeSelected() {
+        document.querySelectorAll('.selected').forEach(function (selected) {
+            selected.remove()
+        })
 
+    }
+    //mover pra baixo
+    let moveUp = document.getElementById("mover-cima")
+    moveUp.addEventListener("click", movUp)
 
-
-
-//remover selecionado
-
-let deleteSelected = document.getElementById("remover-selecionado")
-deleteSelected.addEventListener("click", removeSelected)
-
-function removeSelected() {
-    document.querySelectorAll('.selected').forEach(function(selected){
-        selected.remove()
+    function movUp(){
+        document.querySelectorAll(".selected").forEach(function (selected) {
+            
+            document.getElementById("lista-tarefas").removeChild(selected.nextElementSibling)
+            
         })
     }
 
+     
+    
+} 
+    
 
-  
+
+
+
+
