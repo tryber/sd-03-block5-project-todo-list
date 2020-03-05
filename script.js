7acc9fa7bea5784cba0462d9168e4dcdfdb1ce83
natox <alvares.renato@hotmail.com>
2020-03-05 17:44:53 -0300

    Exclude and half selected workin

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+function newTask() {
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('texto-tarefa').value;
+  let text = document.createTextNode(inputValue);
+  console.log(document.getElementById('lista-tarefas')
+  )
+  let ol = document.getElementById('lista-tarefas');
+  li.onmouseouver;
+  addOverClass(li)
+  li.appendChild(text);
+  ol.appendChild(li);
+  inputValue = "";
+
+}
+
+function addOverClass(x) {
+  x.addEventListener('mouseover', function () {
+
+    x.classList.add('over')
+  })
+  x.addEventListener('mouseout', function () {
+
+    x.classList.remove('over')
+  })
+  x.addEventListener('dblclick', function () {
+
+    x.classList.toggle('selected')
+  })
+
+}
+let criarTask = document.getElementById('criar-tarefa')
+criarTask.addEventListener('click', newTask)
+
+const apagarTudo = document.getElementById('apaga-tudo')
+let li = document.getElementsByTagName('li')
+
+apagarTudo.addEventListener('click', function () {
+  document.getElementById('lista-tarefas').remove();
+  newOl = document.createElement('ol');
+  newOl.id = 'lista-tarefas';
+  console.log(newOl)
+  document.getElementById('container').appendChild(newOl);
+})
+const removerFin = document.getElementById('remover-finalizados')
+let selected = document.getElementsByClassName('selected')
+removerFin.addEventListener('click', function (){
+for (let i in selected){
+  console.log(selected)
+  selected[i].remove()
+  console.log(selected)
+}
+})
