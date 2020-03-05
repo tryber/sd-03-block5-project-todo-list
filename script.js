73267fe3410b17e5d9e63a750baba53b623adc7b
Luma Arruda <luma.oarruda@gmail.com>
2020-03-05 11:00:31 -0300

    criando lista de tarefas

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let text = document.getElementById('texto-tarefa');
 let criar = document.getElementById('criar-tarefa');
-let res = document.getElementById('lista-tarefas');
+let list = document.getElementById('lista-tarefas');
+
+function inserirItem() {
+  let item = document.createElement('li');
+  item.innerText = text.value;
+  console.log(item);
+  
+  list.appendChild(item);
+  text.value = '';
+}
+
+
+window.onload = function () {
+  criar.addEventListener('click', inserirItem);
+}
