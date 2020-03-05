b88cd5474bf85309b8ebfe987086ca682310d732
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-05 13:02:01 -0300

    some req 31.01

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 ct = document.getElementById('criar-tarefa');
 tt = document.getElementById('texto-tarefa');
+at = document.getElementById('apaga-tudo');
 ct.addEventListener('click', criarTarefa);
+at.addEventListener('click', apagaTudo);
 
 function criarTarefa() {
   let li = document.createElement('li');
   document.getElementById('lista-tarefas').appendChild(li);
   li.innerHTML = tt.value;
+  tt.value = '';
 }
+
+function apagaTudo() {
+  let a = document.getElementById('lista-tarefas');
+  while (a.hasChildNodes()) {
+    a.removeChild(a.firstChild);
+  }
+}
+
