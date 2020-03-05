bb456d5fed639242a2051289b1aaf4a263ef0af2
potew <potew0@gmail.com>
2020-03-04 23:00:46 -0300

    Requisitos bônus 1 e 3

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var i, lista = document.getElementById("lista-tarefas");
+let i, lista = document.getElementById("lista-tarefas");
 //document.getElementById("criar-tarefa").addEventListener("click",insereitem());
+
+window.onload = function() {
+    for (i = 0 ; i < localStorage.length ; i++) {
+        let item = document.createElement("li");
+        item.innerText = localStorage.getItem(`I${i}`);
+        item.onclick = "mudaCorFundo()";
+        item.addEventListener('click', function () { selecionaItem(item) });
+        item.addEventListener('dblclick', function () { marcaCompletado(item) });
+        lista.appendChild(item);
+        }
+}
 
 function insereItem() {
     //  Abaixo, um recurso que impede que a função atribuida seja executada junto com os comandos desta
@@ -8,7 +19,7 @@ function insereItem() {
         let item = document.createElement("li");
         item.innerText = textoCaixa;
         item.onclick = "mudaCorFundo()";
-        item.addEventListener('click', function () { mudaCorFundo(item) });
+        item.addEventListener('click', function () { selecionaItem(item) });
         item.addEventListener('dblclick', function () { marcaCompletado(item) });
         lista.appendChild(item);
         document.getElementById("texto-tarefa").value = "";
@@ -18,8 +29,17 @@ function insereItem() {
     document.getElementById("texto-tarefa").focus();
 }
 
-function mudaCorFundo(item) {
-    item.classList.add("cinza");
+function armazenaItens() {
+    let tamLista = lista.childElementCount;
+    for (i = 0 ; i < tamLista ; i++)
+        localStorage.setItem(`I${i}`,lista.childNodes[i].textContent);
+}
+
+function selecionaItem(item) {
+    if (item.classList.contains("cinza"))
+        item.classList.remove("cinza");
+    else
+        item.classList.add("cinza");
 }
 
 function marcaCompletado(item) {
@@ -29,15 +49,21 @@ function marcaCompletado(item) {
         item.classList.add("completed");
 }
 
-//function apagaItem() {
-//    let itemSelecionado
-//}
+function apagaItem() {
+    let itenSelecionados = document.getElementsByClassName("cinza");
+    while(itenSelecionados.length > 0)
+        itenSelecionados[0].remove();
+}
 
 function apagaFinalizados() {
     var listaCompleta = document.getElementsByClassName("completed");
     let elementos = listaCompleta.length;
     while (listaCompleta.length > 0)
         listaCompleta[0].remove();
+}
+
+function apagaLS () {
+    localStorage.clear();
 }
 
 function apagaTudo() {
