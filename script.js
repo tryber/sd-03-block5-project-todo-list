e450e6394a1f1a84b234d1a52aa20f9eddd46dda
luis.campos <gucampos845@gmail.com>
2020-03-05 18:32:41 -0300

    Cursor alterado

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-let botaoCria = document.getElementById('criar-tarefa');
-let botaoApaga = document.getElementById('apaga-tudo')
-let listaOrdenada = document.getElementById('lista-tarefas');
-let texto = document.getElementById('texto-tarefa');
+const botaoCria = document.getElementById('criar-tarefa');
+const botaoApaga = document.getElementById('apaga-tudo');
+const listaOrdenada = document.getElementById('lista-tarefas');
+const texto = document.getElementById('texto-tarefa');
+
+function oneClick() {
+    let tarefas = document.querySelectorAll('.completed');
+
+    for (let i = 0; i < tarefas.length; i += 1) {
+        if (tarefas[i].style.backgroundColor == '#808080') {
+            tarefas[i].addEventListener('click', function () {
+                tarefas[i].style.backgroundColor = '#ffffff';
+            });
+        } else {
+            tarefas[i].addEventListener('click', function () {
+                tarefas[i].style.backgroundColor = '#808080';
+            });
+        }
+    }
+}
+
+function twoClick() {
+    let tarefas = document.querySelectorAll('.completed');
+    for (let i = 0; i < tarefas.length; i += 1) {
+        tarefas[i].addEventListener('dblclick', function () {
+            tarefas[i].style.textDecoration = "line-through"
+        });
+    }
+}
 
 botaoCria.addEventListener('click', function () {
-    let novaTarefa = document.createElement('li');
-    novaTarefa.classList = 'tarefa';
-    let inputTexto = document.getElementById('texto-tarefa').value;
+    const novaTarefa = document.createElement('li');
+    novaTarefa.classList = 'completed';
+    const inputTexto = document.getElementById('texto-tarefa').value;
     novaTarefa.innerHTML = inputTexto;
     listaOrdenada.appendChild(novaTarefa);
-    texto.value = " ";
+    texto.value = ' ';
+
+    oneClick();
+    twoClick();
 });
 
 botaoApaga.addEventListener('click', function () {
     listaOrdenada.innerHTML = ' ';
-})
+});
 
-let tarefas = document.querySelectorAll('.tarefa');
-
-for (let i in tarefas.length) {
-    tarefas[i].addEventListener('click', function () {
-        tarefas[i].style.backgroundColor = "#808080"
-    })
-    tarefas[i].addEventListener('dblclick', function () {
-        
-    })
-}
