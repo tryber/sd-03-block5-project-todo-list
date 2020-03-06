d8c06ab3d17aee5560afb394ccf6f48984e8298d
RafaelPBarbosa <rafael.coper@outlook.com>
2020-03-06 15:52:39 -0300

    Falta botão de apagar

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 
+
 const criarTarefa = document.getElementById("criar-tarefa");
 
 let textoTarefa = document.getElementById("texto-tarefa")
 
+let ol = document.getElementById("lista-tarefas")
+
 criarTarefa.addEventListener("click", function(){
-    textoTarefa = document.getElementById("texto-tarefa").value
+    //textoTarefa = document.getElementById("texto-tarefa").value
     let li = document.createElement("li")
-    let ol = document.getElementById("lista-tarefas")
-    li.textContent = textoTarefa
+    li.textContent = textoTarefa.value
     ol.appendChild(li)
     //textoTarefa.value = ""  -> assim não deu
+    li.addEventListener("click",function(){
+    selected(li)
+    })
+    li.addEventListener('dblclick', function(){
+    completed(li)
+    })
     document.getElementById("texto-tarefa").value = ""
+    
 })
+function selected (x){
+    let remove = document.getElementsByClassName("selected")
+    if ( remove.length > 0 && x.classList.contains("selected")){
+        x.classList.remove("selected")
+    } 
+    else if (remove.length > 0){
+        let remove = document.getElementsByClassName("selected")
+        remove[0].classList.remove("selected")
+        x.classList.add("selected")
+    } 
+    else if (remove.length == 0){
+        x.classList.add("selected")
+    }
+    
+}
+function completed (li){
+    if (li.classList.contains("completed")){
+        li.classList.remove("completed")
+    }
+    else {li.classList.add("completed")}
+
+}
+
+
