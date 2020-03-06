243ad46289bae4320cfcde0fe387f0f941395422
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-06 10:05:26 -0300

    add button clear list

--- script.js
+++ script.js
@@ -3,27 +3,30 @@ let txtTarefa = document.getElementById("criar-tarefa");
 let listando = document.getElementById("lista-tarefas");
 
 txtTarefa.addEventListener("click", addinput);
+let apagaLista = document.querySelector("#apaga-tudo");
 
 function addinput () {
     let list = document.createElement("li");
     list.className = "seta"
     let iten = txtInput.value
     let addIten = document.createTextNode(iten);
-    list.appendChild(addIten)   ;
+    list.appendChild(addIten);
     listando.appendChild(list);
     txtInput.value = ""
     txtInput.focus ()
 }
 
 listando.addEventListener("click", function (backg) {
-    let fundo = backg.target; 
+    let fundo = backg.target;
     fundo.className = "marcado"
-
 })
 
 listando.addEventListener("dblclick", function (event) {
-    let marc = event.target; 
-    marc.className = "completed"
+    let marc = event.target;
+    marc.className = "marcado completed"
+})
 
+apagaLista.addEventListener("click", function limpa () {
+    listando.innerHTML = "";
 })
 
