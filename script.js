815abab114503e975cb3d3f0f0bfde25da08b385
Clayton Pereira <cpereiramt@gmail.com>
2020-03-05 20:14:43 -0400

    Funcionalidade de clique na lista para mudar a cor de fundo

--- script.js
+++ script.js
@@ -2,23 +2,24 @@
 let botaoCriarTarefa = document.getElementById('criar-tarefa');
 let textoInput = document.getElementById('texto-tarefa');  
 let listaTarefas = document.getElementById('lista-tarefas');
-
+let itemList;
 botaoCriarTarefa.addEventListener('click', function (){ 
   if(textoInput.value != ""){  
-   let itemList = document.createElement('li'); 
-   itemList.addEventListener('click',  mudaCor);   
-  listaTarefas.appendChild(itemList).innerHTML = textoInput.value;
-  console.log(document.getElementById('lista-tarefas'));
-  textoInput.value ="";
+    itemList = document.createElement('li'); 
+    itemList.addEventListener('click',  mudaCor);   
+    listaTarefas.appendChild(itemList).innerHTML = textoInput.value;
+    console.log(document.getElementById('lista-tarefas'));
+    textoInput.value ="";
   }else{
    alert('Favor digitar um valor valido !');
   }
 })
 
 function mudaCor(evento){
-  if(evento.target.style.backgroundColor == 'rgb(128,128,128)'){
+  let color = event.target.style.backgroundColor;
+  console.log(color);
+  if(color == 'rgb(128, 128, 128)'){
   evento.target.style.backgroundColor='rgb(255,255,255)';
-  } else {
-
+  } else if(evento.target.style.backgroundColor != 'rgb(128, 128, 128)'){
     evento.target.style.backgroundColor='rgb(128,128,128)';
   }}
