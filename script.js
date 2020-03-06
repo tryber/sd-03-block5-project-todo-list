8a454b837333a160ec7cecdd09261b83f681ff28
Clayton Pereira <cpereiramt@gmail.com>
2020-03-05 20:05:00 -0400

    Funcionalidade de clique na lista para mudar a cor de fundo

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ let textoInput = document.getElementById('texto-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
 
 botaoCriarTarefa.addEventListener('click', function (){ 
-  if(textoInput.value != ""){     
-  listaTarefas.appendChild(document.createElement('li')).innerHTML = textoInput.value;
+  if(textoInput.value != ""){  
+   let itemList = document.createElement('li'); 
+   itemList.addEventListener('click',  mudaCor);   
+  listaTarefas.appendChild(itemList).innerHTML = textoInput.value;
   console.log(document.getElementById('lista-tarefas'));
   textoInput.value ="";
   }else{
    alert('Favor digitar um valor valido !');
-
   }
 })
+
+function mudaCor(evento){
+  if(evento.target.style.backgroundColor == 'rgb(128,128,128)'){
+  evento.target.style.backgroundColor='rgb(255,255,255)';
+  } else {
+
+    evento.target.style.backgroundColor='rgb(128,128,128)';
+  }}
