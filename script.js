72f252385701b5a6e4f9bc76b2615e74e0e8936f
Ga Pimentel <cadgap@gmail.com>
2020-03-07 13:51:50 -0300

    item 7, style especificado para hover

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const botao = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 
-function funAdicionar(event) {
-  let tagli = document.createElement('li');
+function funAdicionar() {
+  const tagli = document.createElement('li');
   tagli.innerHTML = input.value;
   lista.appendChild(tagli);
   input.value = '';
-};
+}
 
 botao.addEventListener('click', funAdicionar);
