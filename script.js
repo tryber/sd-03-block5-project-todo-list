f02d026def41f9f60dabbe26d05d59232fc1d903
Thiago Tavares <thiagooo15@gmail.com>
2020-03-07 16:06:52 -0300

    texte

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-const listaTarefa = document.getElementById("lista-tarefas");
-const tarefa = document.getElementById("texto-tarefa");
-const limpar = document.getElementById('apaga-tudo')
+function addOverClass(x) {
+  x.addEventListener('click', function () {
 
-function addTarefa() {
-  const lista = document.createElement("li");
-  lista.className = "cursor"
-  listaTarefa.appendChild(lista, tarefa);
-  lista.innerText = tarefa.value;
-  tarefa.value = "";
+    x.classList.add('over');
+  });
+/*   x.addEventListener('mouseout', function () {
+    x.classList.remove('over');
+  }); */
+  x.addEventListener('dblclick', function () {
+
+    x.classList.toggle('completed');
+  });
+/*   x.addEventListener('click', function(){
+    x.remove()
+  }) */
+
 }
 
-function limpaLita(){
-  for (let i = 0; i<listaTarefa.childNodes.length; i += 0){
-    listaTarefa.removeChild(listaTarefa.childNodes[i])
+function newTask() {
+  const li = document.createElement('li');
+  let inputValue = document.getElementById('texto-tarefa');
+  const text = document.createTextNode(inputValue.value);
+  const ol = document.getElementById('lista-tarefas');
+  li.onmouseouver;
+  addOverClass(li);
+  li.appendChild(text);
+  ol.appendChild(li);
+  document.getElementById('texto-tarefa').value = '';
+
+
+}
+
+let criarTask = document.getElementById('criar-tarefa');
+criarTask.addEventListener('click', newTask);
+
+const apagarTudo = document.getElementById('apaga-tudo');
+let li = document.getElementsByTagName('li');
+
+apagarTudo.addEventListener('click', function () {
+  document.getElementById('lista-tarefas').remove();
+  let newOl = document.createElement('ol');
+  newOl.id = 'lista-tarefas';
+  document.getElementById('container').appendChild(newOl);
+
+});
+const removerFin = document.getElementById('remover-finalizados');
+
+removerFin.addEventListener('click', function () {
+  let completed = document.querySelectorAll('.completed');
+  for (let i in completed) {
+    completed[i].remove();
   }
-}
+});
