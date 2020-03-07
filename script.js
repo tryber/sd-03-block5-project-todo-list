a79ab60e09330f0412b3170838a3b942e5209ff0
Ga Pimentel <cadgap@gmail.com>
2020-03-07 20:43:12 -0300

    Item 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const botao = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
+let lis = document.querySelectorAll('li');
 
 function funAdicionar() {
   const tagli = document.createElement('li');
   tagli.innerHTML = input.value;
   lista.appendChild(tagli);
+  tagli.addEventListener('click', funBackground);
   input.value = '';
+}
+
+function funBackground(event) {
+  event.target.classList.add('selecionada');
 }
 
 botao.addEventListener('click', funAdicionar);
