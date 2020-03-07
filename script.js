37d0b163f8db6f2c8c0db71595c4b506b42ebfec
Thiago Tavares <thiagooo15@gmail.com>
2020-03-07 20:32:09 -0300

    iniciando requisito 7

--- script.js
+++ script.js
@@ -16,4 +16,29 @@ function limpaLita(){
   }
 }
 
+function bgGray(){
+  const lista = document.createElement("li");
+  for (let i = 0; i < lista.length; i += 1){
+    lista[i].classList.remove('rover')
+  }
 
+}
+
+function mudaSelected(c) {
+
+  for (let i = 0; i < coresArr.length; i += 1) {
+    coresArr[i].classList.remove("selected");
+  }
+
+  const name = "selected";
+  const arr = coresArr[c].className.split(" ");
+  if (arr.indexOf(name) === -1) {
+    coresArr[c].className += " " + name;
+  }
+  //pegar o valor da cor
+  const color = document.querySelector(".selected");
+  const style = getComputedStyle(color);
+  const backgroundColor = style.backgroundColor;
+  let corAtual = backgroundColor;
+  //talve deletar entre esses coment
+}
