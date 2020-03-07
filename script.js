be4da3e7ff48fab6e041516310ade6a1b2eed6a5
henrique <hbaetaleite@gmail.com>
2020-03-07 00:26:05 -0300

    Corrigindo add fundo cinza

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ function listarTarefas() {
   item1.textContent = texto.value;
   item1.classList.add('listaTarefa');
   document.getElementById('texto-tarefa').value = '';
+
+  item1.addEventListener('click', function(){
+      item1.classList.add('selected');
+    }
+  );
+
+  item1.addEventListener('dblclick', function(){
+    item1.classList.add('completed');
+  });
 }
 
 botaoTarefa.addEventListener('click', listarTarefas);
@@ -21,27 +30,3 @@ function apagarTudo() {
 }
 
 botaoApagaTudo.addEventListener('click', apagarTudo);
-
-const lis = document.getElementsByClassName('listaTarefa');
-
-function addFundoCinza() {
-  for (let l = 0; l < lis.length; l += 1) {
-  lis[l].classList.add('selected');
-  }
-};
-
-lista.addEventListener('click', addFundoCinza);
-
-function addRiscado() {
-  for (let l = 0; l < lis.length; l += 1) {
-  lis[l].classList.add('completed');
-  }
-}
-
-function removeRiscado() {
-  for (let = 0; l < lis.length; l += 1) {
-    lis[l].classList.remove('completed');
-  }
-}
-
-lista.addEventListener('dblclick', addRiscado);
