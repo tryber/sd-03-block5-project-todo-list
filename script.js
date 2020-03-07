76c299cbe4ee375e73363ec5835ba766dfd58e88
Thiago Tavares <thiagooo15@gmail.com>
2020-03-07 20:53:06 -0300

    arrumando coisas

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const listaTarefa = document.getElementById("lista-tarefas");
 const tarefa = document.getElementById("texto-tarefa");
-const limpar = document.getElementById('apaga-tudo')
+const limpar = document.getElementById("apaga-tudo");
 
 function addTarefa() {
   const lista = document.createElement("li");
-  lista.className = "cursor item"
+  lista.className = "cursor item";
   listaTarefa.appendChild(lista, tarefa);
   lista.innerText = tarefa.value;
   tarefa.value = "";
-  const items = document.getElementsByClassName('item');
+  const items = document.getElementsByClassName("item");
   for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', bgGray)
+    items[i].addEventListener("click", bgGray);
   }
 }
 
-function limpaLita(){
-  for (let i = 0; i<listaTarefa.childNodes.length; i += 0){
-    listaTarefa.removeChild(listaTarefa.childNodes[i])
+function limpaLita() {
+  for (let i = 0; i < listaTarefa.childNodes.length; i += 0) {
+    listaTarefa.removeChild(listaTarefa.childNodes[i]);
   }
 }
 
-function bgGray(){
-  event.target.className ='grey'
+function bgGray() {
+  event.target.className = "grey";
 }
-
