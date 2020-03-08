807b3d68839dce8d997d3576b760bb9bff5b0829
Ga Pimentel <cadgap@gmail.com>
2020-03-08 16:40:44 -0300

    Item 11, remover finalizados

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const botaoCriar = document.querySelector('#criar-tarefa');
-const botaoApagar = document.querySelector('#apaga-tudo');
-const input = document.querySelector('#texto-tarefa');
-const lista = document.querySelector('#lista-tarefas');
-
 function funBackground(event) {
   event.target.classList.toggle('selecionada');
 }
-
 function funRiscado(event) {
   event.target.classList.toggle('completed');
 }
-
+const botaoCriar = document.querySelector('#criar-tarefa');
+const lista = document.querySelector('#lista-tarefas');
+const input = document.querySelector('#texto-tarefa');
 function funAdicionar() {
   const tagli = document.createElement('li');
   tagli.innerHTML = input.value;
@@ -19,10 +15,21 @@ function funAdicionar() {
   tagli.addEventListener('dblclick', funRiscado);
   input.value = '';
 }
+botaoCriar.addEventListener('click', funAdicionar);
 
 function funApagar() {
   lista.innerHTML = '';
 }
-
-botaoCriar.addEventListener('click', funAdicionar);
+const botaoApagar = document.querySelector('#apaga-tudo');
 botaoApagar.addEventListener('click', funApagar);
+
+function funApagaRiscado() {
+  const lis = document.querySelectorAll('li');
+  for (let i = 0; i < lis.length; i += 1) {
+    if (lis[i].classList.contains('completed') === true) {
+      lis[i].remove();
+    }
+  }
+}
+const botaoApagarRiscado = document.querySelector('#remover-finalizados');
+botaoApagarRiscado.addEventListener('click', funApagaRiscado);
