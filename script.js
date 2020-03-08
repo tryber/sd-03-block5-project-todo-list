f5f762d14197aa8dc23a5963b55b6c0bb2967675
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-03-08 15:33:09 -0300

    colocando botão para limpar

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var botao = document.getElementById("criar-tarefa")
-var lista = document.getElementById("lista-tarefas")
-var novoItem = document.getElementById("texto-tarefa")
+var botaoAdd = document.getElementById("criar-tarefa");
+var lista = document.getElementById("lista-tarefas");
+var novoItem = document.getElementById("texto-tarefa");
+var botaoLimpar = document.getElementById("apaga-tudo");
 
 
-botao.addEventListener('click', function(){
+botaoAdd.addEventListener('click', function(){
     var newLi = document.createElement("li");
     newLi.appendChild(document.createTextNode("algo"));
     lista.appendChild(newLi);
     newLi.innerHTML = novoItem.value;
     novoItem.value = ""
 })
+
+botaoLimpar.addEventListener('click', () => {
+    document.querySelectorAll('li').forEach(function (e) { e.remove(); });
+  });
