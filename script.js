98f7a09da0ef026282cb95f115c48cb2fdde4709
Ga Pimentel <cadgap@gmail.com>
2020-03-07 21:41:38 -0300

    Item 9, com toggle

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const input = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 let lis = document.querySelectorAll('li');
 
+function funBackground(event) {
+  event.target.classList.toggle('selecionada');
+}
+
+function funRiscado(event) {
+  event.target.classList.toggle('completed');
+}
+
 function funAdicionar() {
   const tagli = document.createElement('li');
   tagli.innerHTML = input.value;
@@ -10,14 +18,6 @@ function funAdicionar() {
   tagli.addEventListener('click', funBackground);
   tagli.addEventListener('dblclick', funRiscado);
   input.value = '';
-}
-
-function funBackground(event) {
-  event.target.classList.add('selecionada');
-}
-
-function funRiscado(event) {
-  event.target.classList.add('completed');
 }
 
 botao.addEventListener('click', funAdicionar);
