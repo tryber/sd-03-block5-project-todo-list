5a6ebff3f73c31805250226d5f22e98ff7bd0048
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-08 15:20:33 -0300

    corridindo climate: havia 42 erros

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const botoes = document.querySelectorAll('.botao-model');
 const caixaDeTexto = document.querySelector('#texto-tarefa');
 const listaDeTarefas = document.querySelector('#lista-tarefas');
-const divDaLista = document.getElementById('background-list');
 const tarefas = document.querySelector('ol').childNodes;
 let listSize = 0;
-const completados = document.querySelectorAll('.completed');
 botoes[0].addEventListener('click', function () {
   listSize += 1;
-  console.log(listSize);
   const tarefa = document.createElement('li');
   tarefa.className = 'item';
   listaDeTarefas.appendChild(tarefa);
@@ -16,9 +13,9 @@ botoes[0].addEventListener('click', function () {
   for (let i = 1; i <= listSize; i += 1) {
     tarefas[i].addEventListener('dblclick', function () {
       tarefas[i].className = 'item completed';
-    })
+    });
   }
-})
+});
 for (let i = 0; i < botoes.length; i += 1) {
     botoes[i].addEventListener('mouseover', function () {
         botoes[i].style.cursor = 'pointer';
@@ -26,15 +23,15 @@ for (let i = 0; i < botoes.length; i += 1) {
 }
 botoes[0].addEventListener('click', function (){
   for (let i = 1; i <= listSize; i += 1) {
-    tarefas[i].addEventListener('click', function (){
-      tarefas[i].style.backgroundColor = 'grey';    
-    })
+    tarefas[i].addEventListener('click', function () {
+      tarefas[i].style.backgroundColor = 'grey'; 
+    });
   }
-})
+});
 botoes[1].addEventListener('click', function () {
-  for (let i = 0; i < listSize;  i += 1){
+  for (let i = 0; i < listSize; i += 1) {
     let lista = document.querySelectorAll('li');
     lista[0].remove();
   }
   listSize = 0;
-})
+});
