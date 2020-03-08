b71fac310eb4729cf33736941e0ef2a88f81b03a
William Igor <williamigorsilva@gmail.com>
2020-03-07 22:13:24 -0300

    Ajustando codeCLimate

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 window.onload = function () {
+  const textoTarefa = document.getElementById('texto-tarefa');
+  const criarTarefas = document.getElementById('criar-tarefa');
+  const listaTarefa = document.getElementById('lista-tarefas');
+  let li = document.createElement('li');
+  
 
-  let textoTarefa = document.getElementById("texto-tarefa");
-  let criarTarefas = document.getElementById("criar-tarefa");
-  let listaTarefa = document.getElementById("lista-tarefas");
-  var li = document.createElement('li');
-
-  //incluindo li na ol
+  // incluindo li na ol
 
   function listagem() {
-    let li = document.createElement("li");
+    let li = document.createElement('li');
     listaTarefa.appendChild(li);
     li.textContent = textoTarefa.value;
-    document.getElementById("texto-tarefa").value = "";
+    document.getElementById('texto-tarefa').value = '';
   }
-  criarTarefas.addEventListener("click", listagem)
+  criarTarefas.addEventListener('click', listagem)
 
   function apagarLista() {
     while (listaTarefa.firstChild)
       listaTarefa.removeChild(listaTarefa.lastChild);
+  }
 
-  };
-  let apagaTudo = document.getElementById("apaga-tudo");
-  apagaTudo.addEventListener("click", apagarLista);
+  const apagaTudo = document.getElementById('apaga-tudo');
+  apagaTudo.addEventListener('click', apagarLista);
 
-  let riscalista = li;
+  const riscalista = li;
 
-  riscalista.addEventListener("click", function () {
-    riscalista.style.backgroundColor = 'rgb(128,128,128)'
+  riscalista.addEventListener('click', function () {
+    riscalista.style.backgroundColor = 'rgb(128,128,128)';
   });
 
-  riscalista.addEventListener("dblclick", function () {
-    if (li.classList.contains("completed")) {
-      li.classList.remove("completed")
+  riscalista.addEventListener('dblclick', function () {
+    if (li.classList.contains('completed')) {
+      li.classList.remove('completed')
     }
     else {
-      li.classList.add("completed");
+      li.classList.add('completed');
     }
-  })
-
-}
-
-
+  });
+};
