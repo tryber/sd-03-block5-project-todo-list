25a6bdabc29eba9a8a0bdd9e1a6fa1614c6106f4
William Igor <williamigorsilva@gmail.com>
2020-03-07 21:59:57 -0300

    Adicionando class completed

--- script.js
+++ script.js
@@ -1,52 +1,42 @@
-window.onload = function(){
-  
+window.onload = function () {
+
   let textoTarefa = document.getElementById("texto-tarefa");
   let criarTarefas = document.getElementById("criar-tarefa");
   let listaTarefa = document.getElementById("lista-tarefas");
-  let ol = document.createElement("li");
-  
-  
+  var li = document.createElement('li');
+
   //incluindo li na ol
 
-  function listagem (){
-    let ol = document.createElement("li");
-    listaTarefa.appendChild (ol);
-    ol.textContent = textoTarefa.value;   
+  function listagem() {
+    let li = document.createElement("li");
+    listaTarefa.appendChild(li);
+    li.textContent = textoTarefa.value;
     document.getElementById("texto-tarefa").value = "";
   }
-  criarTarefas.addEventListener("click",listagem)
+  criarTarefas.addEventListener("click", listagem)
 
-  /*
-  function mouseColor(){
-    document.getElementsByTagName("li");
-    ol.style.backgroundColor = "rgb (128,128,128)";  
-  }
-  //ol.addEventListener("click",mouseColor);
-  
-  mouseColor(listed) */
-
-  function apagarLista(){
-    while(listaTarefa.firstChild)
-    listaTarefa.removeChild(listaTarefa.lastChild);
+  function apagarLista() {
+    while (listaTarefa.firstChild)
+      listaTarefa.removeChild(listaTarefa.lastChild);
 
   };
   let apagaTudo = document.getElementById("apaga-tudo");
-  apagaTudo.addEventListener("click",apagarLista);
+  apagaTudo.addEventListener("click", apagarLista);
 
+  let riscalista = li;
 
-  ol.addEventListener("click", function bg(){
-  ol.style.backgroundColor = 'rgb(128, 128, 128)';
-});
+  riscalista.addEventListener("click", function () {
+    riscalista.style.backgroundColor = 'rgb(128,128,128)'
+  });
 
-  ol.addEventListener("dblclick", function riscaLinha(){
-     if (ol.style.textDecoration == "line-through"){
-       ol.style.textDecoration = 'none';
-     }
-     else {
-       ol.style.textDecoration = 'line-through';
-     }
+  riscalista.addEventListener("dblclick", function () {
+    if (li.classList.contains("completed")) {
+      li.classList.remove("completed")
+    }
+    else {
+      li.classList.add("completed");
+    }
   })
-
 
 }
 
