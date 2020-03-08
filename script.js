067a100769f49567444a152d3ab395cccd41074e
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-08 15:03:48 -0300

    corrigindo erros do codeclimate. Havia 119

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-const botoes = document.querySelectorAll(".botao-model");
-let caixaDeTexto = document.querySelector("#texto-tarefa");
-let listaDeTarefas = document.querySelector("#lista-tarefas");
-let divDaLista = document.getElementById("background-list");
+const botoes = document.querySelectorAll('.botao-model');
+const caixaDeTexto = document.querySelector('#texto-tarefa');
+const listaDeTarefas = document.querySelector('#lista-tarefas');
+const divDaLista = document.getElementById('background-list');
+const tarefas = document.querySelector('ol').childNodes;
 let listSize = 0;
-let completados = document.querySelectorAll(".completed"); 
-botoes[0].addEventListener("click", function(){
-  listSize++;
+const completados = document.querySelectorAll('.completed');
+botoes[0].addEventListener('click', function () {
+  listSize += 1;
   console.log(listSize);
-  let tarefa = document.createElement("li");
-  tarefa.className = "item";
+  const tarefa = document.createElement('li');
+  tarefa.className = 'item';
   listaDeTarefas.appendChild(tarefa);
   tarefa.innerHTML = caixaDeTexto.value;
   caixaDeTexto.value = '';
-  for (let i = 1; i <= listSize; i++) {
-    tarefas[i].addEventListener("dblclick", function() {
-      tarefas[i].className = "item completed";
+  for (let i = 1; i <= listSize; i += 1) {
+    tarefas[i].addEventListener('dblclick', function () {
+      tarefas[i].className = 'item completed';
     })
   }
 })
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].addEventListener("mouseover", function() {
-        botoes[i].style.cursor = "pointer";
+for (let i = 0; i < botoes.length; i += 1) {
+    botoes[i].addEventListener('mouseover', function () {
+        botoes[i].style.cursor = 'pointer';
     })
 }
-let tarefas = document.querySelector("ol").childNodes;
-botoes[0].addEventListener("click", function(){
-  for(let i = 1; i <= listSize; i++){
-    tarefas[i].addEventListener("click", function(){
-      tarefas[i].style.backgroundColor = "grey";    
+botoes[0].addEventListener('click', function (){
+  for (let i = 1; i <= listSize; i += 1) {
+    tarefas[i].addEventListener('click', function (){
+      tarefas[i].style.backgroundColor = 'grey';    
     })
   }
 })
-botoes[1].addEventListener("click", function(){
-  for(let i = 0; i < listSize;  i++){
-    let lista = document.querySelectorAll("li");
+botoes[1].addEventListener('click', function () {
+  for (let i = 0; i < listSize;  i += 1){
+    let lista = document.querySelectorAll('li');
     lista[0].remove();
   }
   listSize = 0;
 })
-
-
-
-
-
