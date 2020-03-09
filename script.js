8018b91861c2bc53ae4d91520a80d7ec9703a37e
natox <alvares.renato@hotmail.com>
2020-03-09 16:58:08 -0300

    CC changes

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
 function addOverClass(x) {
   x.addEventListener('click', function () {
-
     x.classList.add('over');
   });
   x.addEventListener('dblclick', function () {
-
     x.classList.toggle('completed');
   });
-
-
 }
 
 function newTask() {
   const li = document.createElement('li');
-  let inputValue = document.getElementById('texto-tarefa');
+  const inputValue = document.getElementById('texto-tarefa');
   const text = document.createTextNode(inputValue.value);
   const ol = document.getElementById('lista-tarefas');
-  li.onmouseouver;
   addOverClass(li);
   li.appendChild(text);
   ol.appendChild(li);
   document.getElementById('texto-tarefa').value = '';
-
-
 }
 
-let criarTask = document.getElementById('criar-tarefa');
+const criarTask = document.getElementById('criar-tarefa');
 criarTask.addEventListener('click', newTask);
 
 const apagarTudo = document.getElementById('apaga-tudo');
-let li = document.getElementsByTagName('li');
 
 apagarTudo.addEventListener('click', function () {
   document.getElementById('lista-tarefas').remove();
   let newOl = document.createElement('ol');
   newOl.id = 'lista-tarefas';
   document.getElementById('container').appendChild(newOl);
-
 });
 
 const removerFin = document.getElementById('remover-finalizados');
 removerFin.addEventListener('click', function () {
-  let completed = document.querySelectorAll('.completed');
-  for (let i in completed) {
+  const completed = document.querySelectorAll('.completed');
+  for (i in completed) {
     completed[i].remove();
   }
 });
 
 document.getElementById('remover-selecionado').addEventListener('click',function(){
-  let selecionado = document.querySelectorAll('.over');
-    for (let i in selecionado) {
-      selecionado[i].remove();
+  const selecionado = document.querySelectorAll('.over');
+  for (i in selecionado) {
+    selecionado[i].remove();
     }
   })
 
