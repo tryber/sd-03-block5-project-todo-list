816f3a2e2e3b5531c10faeea193560a80e68fc8b
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-03-09 19:08:49 -0300

    Marcado de cinza

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function addLi(){
     let tamanho = document.getElementById('texto-tarefa').value.length
     let listNode = document.getElementById('lista-tarefas')
     let liNode = document.createElement("LI")
+    liNode.classList = 'completed'
     let txtNode = document.createTextNode(txtval)
-    
+
     if (tamanho > 0) {
         liNode.appendChild(txtNode);
         listNode.appendChild(liNode);
         value_del.value = ""
+        bgcinza()
     } else {
         window.alert('Adicione uma terefa corretamente')
     }
@@ -20,3 +22,18 @@ let eraseBtn = document.querySelector('#apaga-tudo');
 eraseBtn.addEventListener('click', function() {
   listaOrdenada.innerHTML = '';
 });
+
+function bgcinza(){
+    let tarefas = document.querySelectorAll('.completed');
+    for (let i = 0; i < tarefas.length; i += 1) {
+        if (tarefas[i].style.backgroundColor === "#808080") {
+            tarefas[i].addEventListener('click', function () {
+                tarefas[i].style.backgroundColor = '#ffffff';
+            });
+        } else {
+            tarefas[i].addEventListener('click', function () {
+                tarefas[i].style.backgroundColor = '#808080';
+            });
+        }
+    }
+}
