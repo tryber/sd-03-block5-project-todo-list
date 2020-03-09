857cd1daa133202706c25402503f4a205fe308b0
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-03-09 16:19:09 -0300

    terminando funcionalidades

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ var lista = document.getElementById("lista-tarefas");
 var novoItem = document.getElementById("texto-tarefa");
 var botaoLimpar = document.getElementById("apaga-tudo");
 
-
 botaoAdd.addEventListener('click', function(){
     var newLi = document.createElement("li");
-    newLi.appendChild(document.createTextNode("algo"));
+    newLi.addEventListener("dblclick", function() {
+        if (newLi.classList.contains("completed")){
+          newLi.classList.remove("completed");
+        } else { 
+            newLi.classList.add("completed");
+  }})
     lista.appendChild(newLi);
     newLi.innerHTML = novoItem.value;
     novoItem.value = ""
 })
-
-botaoLimpar.addEventListener('click', () => {
+    botaoLimpar.addEventListener('click', () => {
     document.querySelectorAll('li').forEach(function (e) { e.remove(); });
-  });
+        });
+
+        botaoLimpar.addEventListener('dblclick', () => {
+         document.querySelectorAll('li').forEach(function (e) { e.remove(); });});
+ 
+
+
