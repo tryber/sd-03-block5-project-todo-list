f501a90ea004d8f9d136c2fb6aefe87448ab5a72
Cezene <cezenesn@hotmail.com.br>
2020-03-10 16:32:46 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function select() {
+  document.getElementById("elemento").style.backgroundColor = "gray";
+}
 console.log("hello, world")
+function pr() {
+  document.getElementById("elemento").innerHTML = document.getElementById('criar-tarefa').value;
+}
