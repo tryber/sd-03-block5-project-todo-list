4c43edef32c38f456c8dbcf73ea232b59bebd91f
paulo-lins <paulolins100@gmail.com>
2020-03-10 13:37:42 -0300

    new

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 listaDeTarefas = []
-listaDeTarefasMarcadas = []
+lmarcadas = []
 var i = 0
+
+
+
 
 var botaoCriar = document.getElementById('criar-tarefa')
 var olId = document.getElementById("lista-tarefas")
 var completed = document.getElementsByClassName('completed')
 var deleteAll = document.getElementById('apaga-tudo')
-// var riscado = false
-// var backgroundTarefa = 'white'
+var removerFinalizados = document.getElementById('remover-finalizados')
 
 function type() {
 
   var textTask = document.getElementById("texto-tarefa").value;
-  var li = document.createElement("li");
+  li = document.createElement("li");
   var text = document.createTextNode(textTask);
   li.appendChild(text);
   document.getElementById("lista-tarefas").appendChild(li);
   document.getElementById("texto-tarefa").value = '';
   listaDeTarefas.push(li)
   li.id = i
+
     ++i
   console.log('criado a li ' + li)
 
 }
 
-function identifyObj(event) {
-  var pickId = event.target.id;
-  var corDeFundoTarefa = document.getElementById(pickId).style.backgroundColor
-  if (corDeFundoTarefa = 'rgb(128,128,128)') {
-    corDeFundoTarefa = 'white'
-  }
-}
+// function identifyObj(event) {
+//   var pickId = event.target.id;
+//   var corDeFundoTarefa = document.getElementById(pickId).style.backgroundColor
+//   if (corDeFundoTarefa = 'rgb(128,128,128)') {
+//     corDeFundoTarefa = 'white'
+//   }
+// }
 
 function changeColorLi(event) {
       var pegaId = event.target.id;
@@ -49,14 +52,19 @@ function deleteTasks() {
 function riscaTexto(event) {
   var getId = event.target.id;
   document.getElementById(getId).classList.toggle('completed');
+  lmarcadas.push(getId)
+  console.log(getId)
+
 }
 
-function limpaTexto(event) {
-
-  var limpa = event.target.id;
-  var teste = document.getElementById(limpa)
-  limpa.classList.add('incompleta')
+function limpaTexto() {
+document.querySelectorAll('.completed').forEach(function(a){
+  a.remove()
+})
+  
 }
+
+
 
 function pickObj(e) {
   var idSelect = (e.target.id);
@@ -69,3 +77,4 @@ botaoCriar.addEventListener('click', type);
 olId.addEventListener('click', changeColorLi)
 deleteAll.addEventListener('click', deleteTasks)
 olId.addEventListener('dblclick', riscaTexto)
+removerFinalizados.addEventListener('click', limpaTexto)
