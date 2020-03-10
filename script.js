89b9f58ca28455ede9974326cd983bd4b5e91b36
André Ferreira <andrefpires1@gmail.com>
2020-03-10 18:28:37 -0300

    seleção cinza click

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let botao = document.querySelector("#criar-tarefa");
 let exibeLista = document.querySelector("#lista-tarefas");
 let pegaTexto = document.querySelector("#texto-tarefa");
 
+function itemSelecionado(item) {
+    if (item.classList.contains("selected")) {
+        item.classList.remove("selected");
+    } 
+    else {
+        item.classList.add("selected");
+    }
+}
+
 function criaLista(){
 
     let criaLi = document.createElement("li");
