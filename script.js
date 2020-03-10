ca5fb2ae266223525a2e9e4c39207bacf21730f2
Lucas França <luc.rangel90@gmail.com>
2020-03-10 15:16:13 -0300

    adicionei requisitos ao css e js

--- script.js
+++ script.js
@@ -11,4 +11,31 @@ function criaTarefa() {
   limpaCaixa();
   criarLista.addEventListener('click', highTask);
   criarLista.addEventListener('dblclick', check);
-};
+}
+
+function highTask(event) {
+  e.target.classList.toggle('bgcolor');
+}
+
+function check(event) {
+    e.target.classList.toggle('completed');
+  }
+  
+  botaoCriarTarefa.addEventListener('click', criaTarefa);
+  
+  function limpaCaixa() {
+    caixaTexto.value = '';
+  }
+  
+  let eraseBtn = document.querySelector('#apaga-tudo');
+  eraseBtn.addEventListener('click', function() {
+    checkOl.innerHTML = '';
+  });
+  
+  let tedBtn = document.querySelector('#remover-finalizados');
+  tedBtn.addEventListener('click', function() {
+    const elementsToRemove = document.getElementsByClassName('completed');
+    while (elementsToRemove.length > 0) {
+      document.querySelector('#lista-tarefas').removeChild(elementsToRemove[0]);
+    }
+  });
