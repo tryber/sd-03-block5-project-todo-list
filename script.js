c6862f4e2dd24ee8f50b04bf3644a227f03553c4
Luma Arruda <luma.oarruda@gmail.com>
2020-03-10 18:55:32 -0300

    correçoes do code climate

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const text = document.getElementById('texto-tarefa');
 const criar = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 
+function selecionarItem(e) {
+  e.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function inserirItem() {
   const item = document.createElement('li');
   item.innerText = text.value;
@@ -9,11 +13,7 @@ function inserirItem() {
   text.value = '';
 
   item.addEventListener('click', selecionarItem);
-};
-
-function selecionarItem(e) {
-  e.target.style.backgroundColor = 'rgb(128,128,128)';
-};
+}
 
 window.onload = function () {
   criar.addEventListener('click', inserirItem);
