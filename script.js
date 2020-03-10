c482c44e1b710bdfae4f350fc23b64b4ac761057
Luma Arruda <luma.oarruda@gmail.com>
2020-03-10 18:42:54 -0300

    add pointer e correçoes do code climate

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let text = document.getElementById('texto-tarefa');
-let criar = document.getElementById('criar-tarefa');
-let list = document.getElementById('lista-tarefas');
+const text = document.getElementById('texto-tarefa');
+const criar = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
 
 function inserirItem() {
-  let item = document.createElement('li');
+  const item = document.createElement('li');
   item.innerText = text.value;
-  console.log(item);
-  
   list.appendChild(item);
   text.value = '';
 
   item.addEventListener('click', selecionarItem);
-}
+};
 
 function selecionarItem(e) {
-  console.log(e);
-  e.target.style.backgroundColor = 'rgb(128,128,128)';
-}
+  e.tar;get.style.backgroundColor = 'rgb(128,128,128)';
+};
 
 window.onload = function () {
   criar.addEventListener('click', inserirItem);
-}
+};
