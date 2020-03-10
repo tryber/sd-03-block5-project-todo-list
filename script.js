c9b0e7e06101d1a8797754ce395a7eaf1051c783
saturnian99 <saturnian.dsg@gmail.com>
2020-03-10 15:53:54 -0300

    Rearrenged script. Implemented functions for selected tasks, completed tasks and erase all

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 const criar_tarefa = document.querySelector('#criar-tarefa');
 const texto_tarefa = document.querySelector('#texto-tarefa');
 const lista_tarefas = document.querySelector('#lista-tarefas');
+const apaga_tudo = document.querySelector('#apaga-tudo');
+const tarefas = document.getElementsByTagName('li');
+
+// ================================ Botão "Criar Tarefa" ================================
 
 criar_tarefa.addEventListener('click', criarTarefa);
 
-// Função: cria nova tarefa abaixo, e limpa input de texto.
 function criarTarefa() {
     // Cria tarefa
     let tarefa = document.createElement('li');
     lista_tarefas.appendChild(tarefa).innerHTML = texto_tarefa.value;
     // Limpa input
     texto_tarefa.value = '';
-    // Adiciona classe 'cinza' e 'cursor'
-    tarefa.addEventListener('click', tarefaCinza);
+    // Tarefa transforma ponteiro em "mãozinha"
     tarefa.classList.add('cursor');
+    // Eventos para selecionar e riscar tarefa
+    tarefa.addEventListener('click', tarefaCinza);
+    tarefa.addEventListener('dblclick', tarefaFeita);
+
 }
 
+let click_1 = true;
 // Função: ao clicar na tarefa, deixar cinza.
 function tarefaCinza() {
-    this.className = 'selected';
+    if (click_1 == true) {
+        this.classList.add('selected');
+        click_1 = false;
+    } else {
+        this.classList.remove('selected');
+        click_1 = true;
+    }
+}
+
+let click_2 = true;
+// Função: ao clicar 2x na tarefa, riscar.
+function tarefaFeita() {
+    if (click_2 == true) {
+        this.classList.add('completed');
+        click_2 = false;
+    } else {
+        this.classList.remove('completed');
+        click_2 = true;
+    }
+}
+
+// ================================ Botão "Apagar Tudo" ================================
+
+apaga_tudo.addEventListener('click', apagarTudo);
+
+function apagarTudo() {
+    // Função: ao clicar no botão "Apagar Tudo", excluir todas as tarefas.
+    while (lista_tarefas.firstChild) {
+        lista_tarefas.removeChild(lista_tarefas.lastChild);
+
+    }
 }
