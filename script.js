d2bdf0fc0844957ab9cd5df2b844e9f21c3f79ee
henrique <hbaetaleite@gmail.com>
2020-03-09 23:44:14 -0300

    Adicionando botoes remove finalizados e remove selecionados

--- script.js
+++ script.js
@@ -2,17 +2,23 @@
 const texto = document.getElementById('texto-tarefa');
 const botaoTarefa = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
-const botaoApagaTudo = document.getElementById('apaga-tudo');
+const botaoApagaTudo = document.getElementById('clear-board');
+const botaoRemoveFinalizado = document.getElementById('remover-finalizados');
+const botaoRemoveSelecionado = document.getElementById('remover-selecionado');
 
 function listarTarefas() {
   const item1 = document.createElement('li');
   lista.appendChild(item1);
   item1.textContent = texto.value;
-  item1.classList.add('listaTarefa');
   document.getElementById('texto-tarefa').value = '';
 
   item1.addEventListener('click', function(){
-    item1.classList.add('selected');
+    if (item1.classList.contains('selected')) {
+      item1.classList.remove('selected');
+    }
+    else {
+      item1.classList.add('selected');
+    }
     }
   );
 
@@ -25,6 +31,17 @@ function listarTarefas() {
     }
   });
 
+  botaoRemoveFinalizado.addEventListener('click', function () {
+    if (item1.classList.contains('completed')) {
+      lista.removeChild(item1);
+    }
+  });
+
+  botaoRemoveSelecionado.addEventListener('click', function () {
+    if (item1.classList.contains('selected')) {
+      lista.removeChild(item1);
+    }
+  });
 }
 
 botaoTarefa.addEventListener('click', listarTarefas);
