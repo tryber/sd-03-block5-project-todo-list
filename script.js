c2234fda4e99aee9adcec18de85bccf0ad6f7435
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-11 10:12:44 -0300

    remover finalizados

--- script.js
+++ script.js
@@ -3,42 +3,54 @@ const caixaDeTexto = document.querySelector('#texto-tarefa');
 const listaDeTarefas = document.querySelector('#lista-tarefas');
 const tarefas = document.querySelector('ol').childNodes;
 let listSize = 0;
-function finalização() {
-  if (this.className == 'item') {
-    this.className = 'item completed';
-  } else {
-    this.className = 'item';
+window.onload = function () {
+  function finalização() {
+    if (this.className == 'item') {
+      this.className = 'item completed';
+    } else {
+      this.className = 'item';
+    }
   }
-}
-botoes[0].addEventListener('click', function () {
-  listSize += 1;
-  const tarefa = document.createElement('li');
-  tarefa.className = 'item';
-  listaDeTarefas.appendChild(tarefa);
-  tarefa.innerHTML = caixaDeTexto.value;
-  caixaDeTexto.value = '';
-  for (let i = 0; i < listSize; i += 1) {
+  function removerFinalizados() {
+    const finalizados = document.querySelectorAll('.completed');
+    console.log(finalizados);
+    for(let i = 0; i < finalizados.length; i += 1) {
+      finalizados[i].remove();
+    }
+    listSize = listSize - finalizados.length;
+  }
+  botoes[0].addEventListener('click', function () {
+    listSize += 1;
+    const tarefa = document.createElement('li');
+    tarefa.className = 'item';
+    listaDeTarefas.appendChild(tarefa);
+    tarefa.innerHTML = caixaDeTexto.value;
+    caixaDeTexto.value = '';
     const elementos = document.querySelectorAll('li');
-    elementos[i].addEventListener('dblclick', finalização);
-  }
-});
-
-for (let i = 0; i < botoes.length; i += 1) {
-  botoes[i].addEventListener('mouseover', function () {
-    botoes[i].style.cursor = 'pointer';
+    for (let i = 0; i < elementos.length; i += 1) {
+      elementos[i].addEventListener('dblclick', finalização);
+    }
   });
-}
-botoes[0].addEventListener('click', function () {
-  for (let i = 1; i <= listSize; i += 1) {
-    tarefas[i].addEventListener('click', function () {
-      tarefas[i].style.backgroundColor = 'grey';
+
+  for (let i = 0; i < botoes.length; i += 1) {
+    botoes[i].addEventListener('mouseover', function () {
+      botoes[i].style.cursor = 'pointer';
     });
   }
-});
-botoes[1].addEventListener('click', function () {
-  for (let i = 0; i < listSize; i += 1) {
-    const lista = document.querySelectorAll('li');
-    lista[0].remove();
-  }
-  listSize = 0;
-});
+  botoes[0].addEventListener('click', function () {
+    const elementos = document.querySelectorAll('.item');
+    for (let i = 0; i < elementos.length; i += 1) {
+      elementos[i].addEventListener('click', function () {
+        elementos[i].style.backgroundColor = 'grey';
+      });
+    }
+  });
+  botoes[1].addEventListener('click', function () {
+    for (let i = 0; i < listSize; i += 1) {
+      const lista = document.querySelectorAll('li');
+      lista[0].remove();
+    }
+    listSize = 0;
+  });
+  botoes[2].addEventListener('click', removerFinalizados);
+}
