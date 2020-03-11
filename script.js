702cc0b72ff1178049e94a78cd25d2f1374d43a8
renan.andreolle <renandreolle@hotmail.com>
2020-03-11 06:23:28 -0300

    Manipulacao de lista pronta

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function procreator(){
 let oil = document.createElement("li");
+oil.addEventListener("click",swapper);
 document.querySelector("#lista-tarefas").appendChild(oil);
 }
 window.onload = () => {
@@ -7,7 +8,8 @@ document.getElementById("texto-tarefa").addEventListener("change",event => {
     procreator();
     document.getElementById("lista-tarefas").lastChild.innerText = event.target.value
   event.target.value=""})
-
+document.getElementById("apaga-tudo").addEventListener("click",destroyer);
+document.getElementById("remover-finalizados").addEventListener("click",completer)
 /*Element.prototype.insertChildAtIndex = function(child, index) {
     if (!index) index = 0
     if (index >= this.children.length) {
@@ -17,3 +19,33 @@ document.getElementById("texto-tarefa").addEventListener("change",event => {
     }
   }*/
 }
+function swapper(){
+  if(event.target.classList.length == 0){
+    event.target.classList.add("Um");
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+  }
+  else if(event.target.classList.length == 1){
+    event.target.classList.add("completed");
+  
+  }
+  else if(event.target.classList.length == 2){
+    event.target.classList.add("reseter");
+  }
+  else if(event.target.classList.length == 3){
+    event.target.classList.remove("Um");
+    event.target.classList.remove("completed");
+    event.target.classList.remove("reseter");
+  }
+  }
+  function destroyer(){
+    let des = document.getElementById("lista-tarefas");
+    des.innerHTML="";
+  }
+  function completer(){
+    let des = document.getElementById("lista-tarefas");
+    for(let i=des.children.length-1;i>=0;i--){
+      if(des.children[i].classList.contains("completed")){
+        des.removeChild(des.children[i]);
+      }
+    }
+  }
