a2188c2ec5d188c72edda1b409655f54386d6ba9
renan.andreolle <renandreolle@hotmail.com>
2020-03-11 04:35:44 -0300

    Ajustado o requisito 5

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ document.querySelector("#lista-tarefas").insertChildAtIndex(oil,0);
 window.onload = () => {
 document.getElementById("texto-tarefa").addEventListener("change",event => {
     procreator();
-    document.getElementById("lista-tarefas").firstChild.innerText = event.target.value})
+    document.getElementById("lista-tarefas").firstChild.innerText = event.target.value
+  event.target.value=""})
 
 Element.prototype.insertChildAtIndex = function(child, index) {
     if (!index) index = 0
