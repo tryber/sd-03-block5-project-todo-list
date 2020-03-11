f1e2e5d1d18f7a20ddd4c740eb64c43021eeabba
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-11 09:10:05 -0300

    requisito de finalização de tarefa

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const caixaDeTexto = document.querySelector('#texto-tarefa');
 const listaDeTarefas = document.querySelector('#lista-tarefas');
 const tarefas = document.querySelector('ol').childNodes;
 let listSize = 0;
+function finalização() {
+  if (this.className == 'item') {
+    this.className = 'item completed';
+  } else {
+    this.className = 'item';
+  }
+}
 botoes[0].addEventListener('click', function () {
   listSize += 1;
   const tarefa = document.createElement('li');
@@ -10,12 +17,12 @@ botoes[0].addEventListener('click', function () {
   listaDeTarefas.appendChild(tarefa);
   tarefa.innerHTML = caixaDeTexto.value;
   caixaDeTexto.value = '';
-  for (let i = 1; i <= listSize; i += 1) {
-    tarefas[i].addEventListener('dblclick', function () {
-      tarefas[i].className = 'item completed';
-    });
+  for (let i = 0; i < listSize; i += 1) {
+    const elementos = document.querySelectorAll('li');
+    elementos[i].addEventListener('dblclick', finalização);
   }
 });
+
 for (let i = 0; i < botoes.length; i += 1) {
   botoes[i].addEventListener('mouseover', function () {
     botoes[i].style.cursor = 'pointer';
