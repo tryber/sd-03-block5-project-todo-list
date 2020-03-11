891f2c2d9f7a841e79149610288fd438fed0d1ed
Hfreitas <hfreitas86@gmail.com>
2020-03-10 21:21:02 -0300

    Alterações para adequação ao code climate

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
+//Váriaveis globais que fundamentam o funcionamento da lista
 const createTask = document.getElementById('criar-tarefa');
 const eraseTask = document.getElementById('apaga-tudo');
 const eraseSelectedTask = document.getElementById('remover-finalizados');
 const taskList = document.getElementById('lista-tarefas');
 const text = document.getElementById('texto-tarefa');
 
-function oneClick() {
-    let tasks = document.querySelectorAll('.completed');
+//função para transformar o fundo das tarefas já cumpridas
+function singleClick() {
+  let tasks = document.querySelectorAll('.completed');
 
-    for (let i = 0; i < tasks.length; i += 1) {
-        if (tasks[i].style.backgroundColor === "#808080") {
-            tasks[i].addEventListener('click', function () {
-                tasks[i].style.backgroundColor = '#ffffff';
-            });
-        } else {
-            tasks[i].addEventListener('click', function () {
-                tasks[i].style.backgroundColor = '#808080';
-            });
-        }
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].style.backgroundColor === '#808080') {
+      tasks[i].addEventListener('click', function() {
+        tasks[i].style.backgroundColor = '#ffffff';
+      });
+    } else {
+      tasks[i].addEventListener('click', function() {
+        tasks[i].style.backgroundColor = '#808080';
+      });
     }
+  }
+}
+//função para riscar tarefas concluidas
+function doubleClick() {
+  let tasks = document.querySelectorAll('.completed');
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].style.textDecoration === 'line-through') {
+      tasks[i].style.textDecoration = 'none';
+    } else {
+      tasks[i].addEventListener('dblclick', function() {
+        tasks[i].style.textDecoration = 'line-through';
+      });
+    }
+  }
 }
 
-function twoClick() {
-    let tasks = document.querySelectorAll('.completed');
-    for (let i = 0; i < tasks.length; i += 1) {
-        if (tasks[i].style.textDecoration === "line-through") {
-            tasks[i].style.textDecoration = "none";
-        } else {
-            tasks[i].addEventListener('dblclick', function () {
-                tasks[i].style.textDecoration = "line-through";
-            });
-        }
-    }
-}
-
-createTask.addEventListener('click', function () {
-    const newTask = document.createElement('li');
-    newTask.classList = 'completed';
-    const inputText = document.getElementById('texto-tarefa').value;
-    newTask.innerHTML = inputText;
-    taskList.appendChild(newTask);
-    text.value = '';
-    oneClick();
-    twoClick();
+//Event Listeners
+createTask.addEventListener('click', function() {
+  const newTask = document.createElement('li');
+  newTask.classList = 'completed';
+  const inputText = document.getElementById('texto-tarefa').value;
+  newTask.innerHTML = inputText;
+  taskList.appendChild(newTask);
+  text.value = '';
+  singleClick();
+  doubleClick();
 });
 
-eraseTask.addEventListener('click', function () {
-    taskList.innerHTML = '';
+eraseTask.addEventListener('click', function() {
+  taskList.innerHTML = '';
 });
 
-eraseSelectedTask.addEventListener('click', function () {
-    let tasks = document.querySelectorAll('.completed');
-    for (let i = 0; i < tasks.length; i += 1) {
-        if (tasks[i].style.textDecoration === "line-through") {
-            taskList.removeChild(tasks[i]);
-        }
+eraseSelectedTask.addEventListener('click', function() {
+  let tasks = document.querySelectorAll('.completed');
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].style.textDecoration === 'line-through') {
+      taskList.removeChild(tasks[i]);
     }
-})
+  }
+});
