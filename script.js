1fd7098b734b35d42061c769686b0e938e0ba7de
renan.andreolle <renandreolle@hotmail.com>
2020-03-11 04:25:59 -0300

    De volta ao trabalho

---
+++ script.js
@@ -0,0 +1,18 @@
+function procreator(){
+let oil = document.createElement("li");
+document.querySelector("#lista-tarefas").insertChildAtIndex(oil,0);
+}
+window.onload = () => {
+document.getElementById("texto-tarefa").addEventListener("change",event => {
+    procreator();
+    document.getElementById("lista-tarefas").firstChild.innerText = event.target.value})
+
+Element.prototype.insertChildAtIndex = function(child, index) {
+    if (!index) index = 0
+    if (index >= this.children.length) {
+      this.appendChild(child)
+    } else {
+      this.insertBefore(child, this.children[index])
+    }
+  }
+}
