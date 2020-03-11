430cc44ebb29e13b7ae4a47529a2a3e5984b3522
mirellaalves <mirella.alves@gmail.com>
2020-03-11 11:29:05 -0300

    Adicionando funcoes botoes de limpar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let textoDigitado = document.getElementById("texto-tarefa");
-let clickButton = document.querySelector("#criar-tarefa");
+let clickButtonAdicionar = document.querySelector("#criar-tarefa");
 let incluiItem = document.querySelector("#lista-tarefas");
 
 function coloreItemLista(e) {
@@ -8,6 +8,10 @@ function coloreItemLista(e) {
 
 function riscaItemLista(e) {
     e.target.classList.add('completed');
+}
+
+function reverteRiscaItemLista(e) {
+    e.target.classList.remove('completed');
 }
 
 function adicionaItem() {
@@ -19,4 +23,27 @@ function adicionaItem() {
     criaLista.addEventListener("click", coloreItemLista);
     criaLista.addEventListener("dblclick", riscaItemLista);
     };
-clickButton.addEventListener("click", adicionaItem);
+clickButtonAdicionar.addEventListener("click", adicionaItem);
+
+let clickButtonLimpaConcluidos = document.querySelector("#remover-finalizados");
+let clickButtonLimpaTodos = document.querySelector("#apaga-tudo");
+
+/*function limpaRiscados() {
+    let finalizados = document.querySelectorAll(".completed");
+    finalizados.remove();
+}
+clickButtonLimpaConcluidos.addEventListener("click", limpaRiscados);*/
+
+function limpaConcluidos() {
+    for (let i = document.querySelectorAll(".completed").length; i>=0; i--) {
+        document.querySelector(".completed").remove();
+    }
+}
+clickButtonLimpaConcluidos.addEventListener("click", limpaConcluidos);
+
+function limpaTodos() {
+    for (let i = document.querySelectorAll("li").length; i>=0; i--) {
+        document.querySelector("li").remove();
+    }
+}
+clickButtonLimpaTodos.addEventListener("click", limpaTodos);
