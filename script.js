3f27eb26a52593ee46260e1efbce41baafaf2e7a
Luma Arruda <luma.oarruda@gmail.com>
2020-03-11 13:07:03 -0300

    borao remover finalizados e remover selecionados

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const criar = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const btnLimparLista = document.getElementById('apaga-tudo');
 const btnFinalizados = document.getElementById('remover-finalizados');
+const btnSelecionados = document.getElementById('remover-selecionado');
 
 function selecionarItem(e) {
   e.target.classList.toggle('cinza');
@@ -33,8 +34,27 @@ function remover() {
   location.reload();
 }
 
+function removerFinalizados(className) { 
+  let elements = document.getElementsByClassName('completed'); 
+  let first = elements[0]; 
+  while (first) { 
+      first.remove(); 
+      first = elements[0]; 
+  } 
+}
+
+function removerSelecionados(className) { 
+  let elements = document.getElementsByClassName('cinza'); 
+  let first = elements[0]; 
+  while (first) { 
+      first.remove(); 
+      first = elements[0]; 
+  } 
+}
+
 window.onload = function () {
   criar.addEventListener('click', inserirItem);
   btnLimparLista.addEventListener('click', remover);
   btnFinalizados.addEventListener('click', removerFinalizados);
+  btnSelecionados.addEventListener('click', removerSelecionados);
 };
