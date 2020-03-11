dab3a441a4188ef64580434eb9949a80150d5738
mirellaalves <mirella.alves@gmail.com>
2020-03-11 08:16:25 -0300

    Criada lista de tarefas

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let = 
+let textoDigitado = document.getElementById("texto-tarefa");
+let clickButton = document.querySelector("#criar-tarefa");
+let incluiItem = document.querySelector("#lista-tarefas");
+
+function adicionaItem() {
+    let criaLista = document.createElement("li");
+    criaLista.innerHTML = textoDigitado.value;
+    incluiItem.appendChild(criaLista);
+    };
+clickButton.addEventListener("click", adicionaItem);
