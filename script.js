2692400ba261df2c0e36c9e757155a3626aa2bc1
mirellaalves <mirella.alves@gmail.com>
2020-03-11 10:04:52 -0300

    Incluindo funcoes para colorir e riscar itens da lista

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ let textoDigitado = document.getElementById("texto-tarefa");
 let clickButton = document.querySelector("#criar-tarefa");
 let incluiItem = document.querySelector("#lista-tarefas");
 
+function coloreItemLista(e) {
+    e.target.classList.add('cinza');
+}
+
+function riscaItemLista(e) {
+    e.target.classList.add('completed');
+}
+
 function adicionaItem() {
     let criaLista = document.createElement("li");
     criaLista.innerHTML = textoDigitado.value;
     incluiItem.appendChild(criaLista);
+    textoDigitado.value = "";
+
+    criaLista.addEventListener("click", coloreItemLista);
+    criaLista.addEventListener("dblclick", riscaItemLista);
     };
 clickButton.addEventListener("click", adicionaItem);
