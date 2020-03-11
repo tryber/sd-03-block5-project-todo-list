e520fb259325a4de6439af57444ec9710435d89c
renan.andreolle <renandreolle@hotmail.com>
2020-03-11 06:54:47 -0300

    code climate2

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 function swapper() {
-  if (event.target.classList.length == 0) {
+  if (event.target.classList.length === 0) {
     event.target.classList.add('Um');
     event.target.style.backgroundColor = 'rgb(128,128,128)';
-  }
-  else if (event.target.classList.length == 1) {
+  } else if (event.target.classList.length === 1) {
     event.target.classList.add('completed');
-  }
-  else if (event.target.classList.length == 2) {
+  } else if (event.target.classList.length === 2) {
     event.target.classList.add('reseter');
-  }
-  else if (event.target.classList.length == 3) {
+  } else if (event.target.classList.length === 3) {
     event.target.classList.remove('Um');
     event.target.classList.remove('completed');
     event.target.classList.remove('reseter');
   }
 }
 function destroyer() {
-  let des = document.getElementById('lista-tarefas');
+  const des = document.getElementById('lista-tarefas');
   des.innerHTML = '';
 }
 function completer() {
-  let des = document.getElementById('lista-tarefas');
-  for (let i = des.children.length - 1; i >= 0; i--) {
-    if (des.children[i].classList.contains('completed')) {
-      des.removeChild(des.children[i]);
+  const dos = document.getElementById('lista-tarefas');
+  for (let i = dos.children.length - 1; i >= 0; i--) {
+    if (dos.children[i].classList.contains('completed')) {
+      dos.removeChild(dos.children[i]);
     }
   }
 }
@@ -37,7 +34,7 @@ window.onload = () => {
     procreator();
     document.getElementById('lista-tarefas').lastChild.innerText = event.target.value;
     event.target.value = '';
-  })
+  });
   document.getElementById('apaga-tudo').addEventListener('click', destroyer);
   document.getElementById('remover-finalizados').addEventListener('click', completer);
 }
