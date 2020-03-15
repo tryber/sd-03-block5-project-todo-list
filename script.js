3ec8dc302ee0afbad478f1489979d5fed6d282ac
Cezene <cezenesn@hotmail.com.br>
2020-03-15 01:19:30 -0300

    oi

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function adicionarTarefa() {
-
     var text = document.getElementById('texto-tarefa').value; 
     var itemLista = document.createTextNode(text);
     var newItem = document.createElement("li");
@@ -8,4 +7,15 @@ function adicionarTarefa() {
     document.getElementById('texto-tarefa').value= '';
 }
 
+function select () {
+    document.getElementById('item-lista').style.backgroundColor = "gray";
+    var sel = document.getElementsByTagName("li")
+   /*  sel.className += "selected" */
+    sel.classList.add("selected")
+    document.getElementsByClassName('selected').style.color = "red";
+}
+
+function clear () {
+    document.querySelectorAll('#lista-tarefas').innerHTML = "";
+}
 console.log("hello, world")
