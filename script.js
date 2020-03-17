2f3ad133c0e0ad0eebe9134c43bd988f074860e5
Cezene <cezenesn@hotmail.com.br>
2020-03-16 22:07:37 -0300

    push

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
+const lista = document.getElementById('lista-tarefas');
+
 function adicionarTarefa() {
-    var text = document.getElementById('texto-tarefa').value; 
-    var itemLista = document.createTextNode(text);
+    
+    /* var itemLista = document.createTextNode(text); */
     var newItem = document.createElement("li");
-    newItem.appendChild(itemLista);
+    var text = document.getElementById('texto-tarefa').value; 
+    newItem.innerHTML = text;
+   /*  newItem.appendChild(itemLista); */
     document.getElementById('lista-tarefas').appendChild(newItem);
     document.getElementById('texto-tarefa').value= '';
 }
 
-function select () {
-    document.getElementById('item-lista').style.backgroundColor = "gray";
-    var sel = document.getElementsByTagName("li")
-   /*  sel.className += "selected" */
-    sel.classList.add("selected")
-    document.getElementsByClassName('selected').style.color = "red";
-}
-
-function clear () {
-    document.querySelectorAll('#lista-tarefas').innerHTML = "";
-}
 console.log("hello, world")
