6e3c27d72f257c80fa4c56adb71198258378bb48
Cezene <cezenesn@hotmail.com.br>
2020-03-16 22:08:34 -0300

    bla

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const lista = document.getElementById('lista-tarefas');
 
 function adicionarTarefa() {
     
-    /* var itemLista = document.createTextNode(text); */
+   
     var newItem = document.createElement("li");
     var text = document.getElementById('texto-tarefa').value; 
     newItem.innerHTML = text;
-   /*  newItem.appendChild(itemLista); */
     document.getElementById('lista-tarefas').appendChild(newItem);
     document.getElementById('texto-tarefa').value= '';
 }
