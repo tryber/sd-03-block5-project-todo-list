e9c4b999cdbe2ccd5630f64d26ae1310626e04bb
Cezene <cezenesn@hotmail.com.br>
2020-03-24 04:19:08 -0300

    hmm

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const lista = document.getElementById('lista-tarefas');
 let limpar = document.getElementById('apaga-tudo');
 let remover = document.getElementById('remover-finalizados');
 let save = document.getElementById('salvar-tarefas');
-let movUp = document.getElementById('mover-cima');
-let movDown = document.getElementById('mover-baixo');
 let cleanSelect = document.getElementById('remover-selecionado');
 let listaTarefas = document.getElementById('lista-tarefas');
 
@@ -38,33 +36,14 @@ function adicionarTarefa () {
   remover.addEventListener('click',function(){
     document.querySelectorAll('.completed').forEach(e => e.remove());
   })
+  save.addEventListener('click',function(){
+    localStorage.setItem('salvar', listaTarefas.innerHTML);
+  })
+  if(localStorage.getItem('salvar')){
+    listaTarefas.innerHTML = localStorage.getItem('salvar');  
+  }
 }
 
-function bonus() {
-  let salvarTarefas = document.getElementById('salvar-tarefas');
-    let moverCima = document.getElementById('mover-cima');
-    let moverBaixo = document.getElementById('mover-baixo');
-    let removerSelecionado = document.getElementById('remover-selecionado');
-  salvarTarefas.addEventListener('click',function(){
-    localStorage.setItem('salvar', listaTarefas.innerHTML);
-})
-if(localStorage.getItem('salvar')){
-    listaTarefas.innerHTML = localStorage.getItem('salvar');  
-}
-moverCima.addEventListener('click',function(){
-    let children = document.querySelector('#lista-tarefas');
-    let up = document.getElementsByClassName('selected')[0];
-    children.insertBefore(up,up.previousSibling);
-})
-moverBaixo.addEventListener('click',function(){
-    let children = document.querySelector('#lista-tarefas');
-    let up = document.getElementsByClassName('selected')[0];
-    children.insertBefore(up,up.nextSibling.nextSibling);
-})
-removerSelecionado.addEventListener('click',function(){
-    document.querySelectorAll('.selected').forEach(e => e.remove());
-})
-}
 
 
 
