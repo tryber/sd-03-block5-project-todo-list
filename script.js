2fe6f903fd6c6c700840f71f764fc36a635d7ddf
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-04-02 00:59:39 -0300

    remover lista ol

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const input = document.getElementById('texto-tarefa');
 const btnEnviar = document.getElementById('criar-tarefa');
 const addList = document.getElementById('lista-tarefas');
 
+const btnBlackOut = document.getElementById('apaga-tudo');
+
 function trocaCorFundo(e) {
 	const addBackground = document.getElementsByClassName('cinza');
 	
@@ -26,8 +28,17 @@ function riscarItem(e) {
 	e.target.classList.toggle('completed');
 }
 
+function blackout() {
+	const lista = document.getElementsByTagName('ol');
+	
+	for (let i = 0; i < lista.length; i += 1) {
+		lista[i].remove();
+	}
+
+}
 
 window.onload = function () {
 	btnEnviar.addEventListener('click', adicionarlista);
+	btnBlackOut.addEventListener('click', blackout);
 	
 } 
