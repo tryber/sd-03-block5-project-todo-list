6e15531f9581e3a5bbe4e545e050c9935c3939c7
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-04-02 00:27:46 -0300

    adicionar cor gray

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const input = document.getElementById('texto-tarefa');
 const btnEnviar = document.getElementById('criar-tarefa');
 const addList = document.getElementById('lista-tarefas');
 
-window.onload = function () {
-  btnEnviar.addEventListener('click', adicionarlista);
-} 
+function trocaCorFundo(e) {
+	const addBackground = document.getElementsByClassName('cinza');
+	
+	for(let i = 0; i < addBackground.length; i += 1){
+		addBackground[i].classList.remove('cinza');
+	}
+	e.target.classList.add('cinza');
+}
 
 function adicionarlista() {
 	const criarItemLi = document.createElement('li');
@@ -12,5 +17,12 @@ function adicionarlista() {
 	criarItemLi.innerText = input.value;
 	addList.appendChild(criarItemLi);
 	input.value = '';
+
+	criarItemLi.addEventListener('click', trocaCorFundo);
 }
 
+
+window.onload = function () {
+	btnEnviar.addEventListener('click', adicionarlista);
+	
+} 
